33a13148445680d37d72ae4291817cf88064fac0
이빵주 <yungju9982@naver.com>
2022-12-30 15:02:24 +0900

    #3 [feat] 바텀네비게이션 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/main/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.sopt.peekabookaos.presentation.main
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityMainBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
@@ -10,5 +14,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        val navController = findNavController()
+        binding.bnvMain.setupWithNavController(navController)
+    }
+
+    private fun findNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        return navHostFragment.navController
+    }
     }
 }
