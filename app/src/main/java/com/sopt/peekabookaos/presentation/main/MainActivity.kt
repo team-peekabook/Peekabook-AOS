23646a871a1f2ba89eb9e1dab423710b3ae5ec5d
이빵주 <yungju9982@naver.com>
2022-12-30 15:02:46 +0900

    #3 [feat] 안드로이드 백버튼 두 번 클릭시 앱 종료 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sopt.peekabookaos.presentation.main
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -8,13 +9,18 @@ import androidx.navigation.ui.setupWithNavController
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityMainBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
+import com.sopt.peekabookaos.util.extensions.ToastMessageUtil
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private var onBackPressedTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBottomNavigationView()
+        initBackPressedCallback()
     }
 
     private fun initBottomNavigationView() {
@@ -27,5 +33,31 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         return navHostFragment.navController
     }
+
+    private fun initBackPressedCallback() {
+        onBackPressedDispatcher.addCallback {
+            val fragmentId: Int? = findNavController(R.id.fcv_main).currentDestination?.id
+            fragmentId?.also { id ->
+                if (id == R.id.bookshelfFragment) {
+                    val curTime = System.currentTimeMillis()
+                    val gap = curTime - onBackPressedTime
+                    if (gap > WAITING_DEADLINE) {
+                        onBackPressedTime = curTime
+                        ToastMessageUtil.showToast(
+                            this@MainActivity,
+                            getString(R.string.finish_app_toast_msg)
+                        )
+                        return@addCallback
+                    }
+                    finishAffinity()
+                    System.runFinalization()
+                    exitProcess(0)
+                }
+            } ?: Timber.e(getString(R.string.null_point_exception))
+        }
+    }
+
+    companion object {
+        private const val WAITING_DEADLINE = 2000L
     }
 }
