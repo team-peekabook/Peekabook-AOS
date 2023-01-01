76a78f248765d30548435c48bdb9e2c7f6569a67
이빵주 <yungju9982@naver.com>
2023-01-02 01:56:44 +0900

    #5 [add] 뷰모델 데이터바인딩

--- app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookActivity.kt
@@ -1,6 +1,7 @@
 package com.sopt.peekabookaos.presentation.registerBook
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityRegisterBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
@@ -9,7 +10,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class RegisterBookActivity :
     BindingActivity<ActivityRegisterBookBinding>(R.layout.activity_register_book) {
+    private val registerBookViewModel: RegisterBookViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = registerBookViewModel
     }
 }
