8f2c36cb75e54308fc28dce112763cff83543142
이빵주 <yungju9982@naver.com>
2023-01-02 03:35:36 +0900

    #5 [add] 뷰모델 변수 추가 (양방향 데이터바인딩)

--- app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
@@ -8,4 +8,9 @@ import com.sopt.peekabookaos.data.entity.BookRegister
 class RegisterBookViewModel : ViewModel() {
     private val _registerBookData = MutableLiveData<BookRegister>()
     val registerBookData: LiveData<BookRegister> = _registerBookData
+
+    val comment = MutableLiveData("")
+
+    val memo = MutableLiveData("")
+
 }
