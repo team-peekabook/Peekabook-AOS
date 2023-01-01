fe5ba57a562677b4e16f6fbb9f3cd7271be5fca8
이빵주 <yungju9982@naver.com>
2023-01-02 01:52:54 +0900

    #5 [add] BookRegister 데이터클래스 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/BookRegister.kt
@@ -0,0 +1,9 @@
+package com.sopt.peekabookaos.data.entity
+
+data class BookRegister(
+    val img: String,
+    val title: String,
+    val author: String,
+    val comment: String?,
+    val memo: String?
+)
