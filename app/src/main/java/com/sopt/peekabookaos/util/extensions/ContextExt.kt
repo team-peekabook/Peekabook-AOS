3501ebfb010cff609ea60c1077670af5a49b236b
이빵주 <yungju9982@naver.com>
2023-01-02 03:35:52 +0900

    #5 [fix] 싱글클릭리스너 확장함수 네이밍 수정

--- app/src/main/java/com/sopt/peekabookaos/util/extensions/ContextExt.kt
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/ContextExt.kt
@@ -2,6 +2,6 @@ package com.sopt.peekabookaos.util.extensions
 
 import android.view.View
 
-fun View.setOnSingleClickListener(onSingleClick: (View) -> Unit) {
+fun View.setOnClickListener(onSingleClick: (View) -> Unit) {
     setOnClickListener(OnSingleClickListener { onSingleClick(it) })
 }
