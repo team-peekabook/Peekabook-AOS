d83d15f70a6df6e39f8365192382a91254d9163b
이빵주 <yungju9982@naver.com>
2023-01-02 03:37:27 +0900

    #5 [feat] RegisterBook 뷰모델에 더미데이터 삽입

--- app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
@@ -13,4 +13,18 @@ class RegisterBookViewModel : ViewModel() {
 
     val memo = MutableLiveData("")
 
+    init {
+        initRegisterBookData()
+    }
+
+    /* 더미데이터. 추후 서버 통신 시 제거 예정 */
+    private fun initRegisterBookData() {
+        _registerBookData.value = BookRegister(
+            img = "http://image.yes24.com/goods/90365124/XL",
+            title = "아무튼, 여름",
+            author = "김신회",
+            comment = "",
+            memo = ""
+        )
+    }
 }
