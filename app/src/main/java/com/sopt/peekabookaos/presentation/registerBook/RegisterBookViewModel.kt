1a967b93f0dc48f0d36cae49a7cda6614da9defa
이빵주 <yungju9982@naver.com>
2023-01-02 01:53:34 +0900

    #5 [add] RegisterBookViewModel 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
@@ -0,0 +1,7 @@
+package com.sopt.peekabookaos.presentation.registerBook
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class RegisterBookViewModel : ViewModel()
