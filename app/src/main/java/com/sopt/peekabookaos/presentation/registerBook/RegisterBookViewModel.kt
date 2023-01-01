61735e9a7557bb60122a79e6b4ea45d35709892a
이빵주 <yungju9982@naver.com>
2023-01-02 03:35:14 +0900

    #5 [delete] 서버통신 구현 전이라 뷰모델 어노테이션 제거

--- app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sopt.peekabookaos.data.entity.BookRegister
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class RegisterBookViewModel : ViewModel() {
     private val _registerBookData = MutableLiveData<BookRegister>()
     val registerBookData: LiveData<BookRegister> = _registerBookData
