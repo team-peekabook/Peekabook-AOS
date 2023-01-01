e5c0f23f92cf33a30187820ed30118c4a570f41a
이빵주 <yungju9982@naver.com>
2023-01-02 01:36:05 +0900

    #5 [fix] 이슈 변경으로 액티비티명 변경

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.peekabookaos.presentation.registerBook
+
+import android.os.Bundle
+import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.databinding.ActivityRegisterBookBinding
+import com.sopt.peekabookaos.util.binding.BindingActivity
+
+class RegisterBookActivity :
+    BindingActivity<ActivityRegisterBookBinding>(R.layout.activity_register_book) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
