4423293dc33ef9c38703c0e1b672018d39d9bc3f
이빵주 <yungju9982@naver.com>
2023-01-02 01:53:16 +0900

    #5 [add] RegisterBookActivity에 AndroidEntryPoint 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityRegisterBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterBookActivity :
     BindingActivity<ActivityRegisterBookBinding>(R.layout.activity_register_book) {
     override fun onCreate(savedInstanceState: Bundle?) {
