902dcf082b1c40ed46b79e5c10f468c469015c39
이빵주 <yungju9982@naver.com>
2023-01-02 00:00:38 +0900

    #5 [add] BarcodeScanner 액티비티 추가

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/bookRegister/barcode/BarcodeScannerActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.peekabookaos.presentation.bookRegister.barcode
+
+import android.os.Bundle
+import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.databinding.ActivityBarcodeScannerBinding
+import com.sopt.peekabookaos.util.binding.BindingActivity
+
+class BarcodeScannerActivity :
+    BindingActivity<ActivityBarcodeScannerBinding>(R.layout.activity_barcode_scanner) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
