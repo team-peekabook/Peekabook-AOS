9d3f8c4b472d5d05462aaac69d5151f2004b3a82
이빵주 <yungju9982@naver.com>
2023-01-02 01:56:56 +0900

    #5 [add] viewmodel에 변수 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/registerBook/RegisterBookViewModel.kt
@@ -1,7 +1,13 @@
 package com.sopt.peekabookaos.presentation.registerBook
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sopt.peekabookaos.data.entity.BookRegister
 import dagger.hilt.android.lifecycle.HiltViewModel
 
 @HiltViewModel
-class RegisterBookViewModel : ViewModel()
+class RegisterBookViewModel : ViewModel() {
+    private val _registerBookData = MutableLiveData<BookRegister>()
+    val registerBookData: LiveData<BookRegister> = _registerBookData
+}
