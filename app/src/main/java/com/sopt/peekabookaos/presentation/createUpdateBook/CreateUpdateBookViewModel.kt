43deb0626aae45d6f071baff1e28fda93cf2e8ca
이빵주 <yungju9982@naver.com>
2023-01-03 05:23:32 +0900

    #11 [add] 라이브데이터 변수 및 setter 설정

--- app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookViewModel.kt
@@ -8,11 +8,15 @@ class CreateUpdateBookViewModel : ViewModel() {
     private val _createUpdateBookData = MutableLiveData<CreateUpdateBook>()
     val createUpdateBookData: LiveData<CreateUpdateBook> = _createUpdateBookData
 
+    private val _isUpdate = MutableLiveData<Boolean>()
+    val isUpdate: LiveData<Boolean> = _isUpdate
 
     val comment = MutableLiveData("")
 
     val memo = MutableLiveData("")
 
+    fun initIsUpdate(update: Boolean) {
+        _isUpdate.value = update
     }
 
     }
