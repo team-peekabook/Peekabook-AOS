1ee777d953ce0d8e46da7fa24bd3f6bf9172adc0
이빵주 <yungju9982@naver.com>
2023-01-03 02:38:50 +0900

    #11 [fix] RegisterBook에서 CreateUpdateBook으로 변경

--- app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookActivity.kt
@@ -1,16 +1,16 @@
-package com.sopt.peekabookaos.presentation.registerBook
+package com.sopt.peekabookaos.presentation.createUpdateBook
 
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.sopt.peekabookaos.R
-import com.sopt.peekabookaos.databinding.ActivityRegisterBookBinding
+import com.sopt.peekabookaos.databinding.ActivityCreateUpdateBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RegisterBookActivity :
-    BindingActivity<ActivityRegisterBookBinding>(R.layout.activity_register_book) {
-    private val registerBookViewModel: RegisterBookViewModel by viewModels()
+class CreateUpdateBookActivity :
+    BindingActivity<ActivityCreateUpdateBookBinding>(R.layout.activity_create_update_book) {
+    private val registerBookViewModel: CreateUpdateBookViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
