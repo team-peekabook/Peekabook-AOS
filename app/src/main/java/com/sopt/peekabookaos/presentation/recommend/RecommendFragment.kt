43a445e87ced47e3a91d63ad7546dafca473e340
hajeong <happykhj67@naver.com>
2023-01-05 05:46:09 +0900

    #6 [chore] 네이밍 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendFragment.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendFragment.kt
@@ -6,7 +6,9 @@ import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.data.entity.Recommend
 import com.sopt.peekabookaos.databinding.FragmentRecommendBinding
 import com.sopt.peekabookaos.util.binding.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecommendFragment :
     BindingFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
     private lateinit var recommendAdapter: BookRecommendAdapter
