d85c0e62bf017e5c897a29c0ce3844c5c3cbae09
이빵주 <yungju9982@naver.com>
2023-01-05 05:10:26 +0900

    #13 [add] 바코드 뷰모델 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
@@ -0,0 +1,9 @@
+package com.sopt.peekabookaos.presentation.barcodeScanner
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class BarcodeViewModel : ViewModel() {
+}
