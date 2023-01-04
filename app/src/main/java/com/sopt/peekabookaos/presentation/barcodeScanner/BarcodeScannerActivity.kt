51835c699a7e6e9e54c80608400b42bc36fbb848
이빵주 <yungju9982@naver.com>
2023-01-05 04:55:35 +0900

    #13 [add] import

--- app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeScannerActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeScannerActivity.kt
@@ -1,10 +1,41 @@
 package com.sopt.peekabookaos.presentation.barcodeScanner
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraInfo
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.core.UseCase
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.data.entity.Book
 import com.sopt.peekabookaos.databinding.ActivityBarcodeScannerBinding
+import com.sopt.peekabookaos.presentation.createUpdateBook.CreateUpdateBookActivity
+import com.sopt.peekabookaos.presentation.createUpdateBook.CreateUpdateBookActivity.Companion.CREATE
+import com.sopt.peekabookaos.presentation.createUpdateBook.CreateUpdateBookActivity.Companion.LOCATION
 import com.sopt.peekabookaos.util.binding.BindingActivity
+import com.sopt.peekabookaos.util.extensions.ToastMessageUtil
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 @AndroidEntryPoint
 class BarcodeScannerActivity :
