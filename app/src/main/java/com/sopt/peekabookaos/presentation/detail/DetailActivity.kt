ffafd0db9956187b3b0e81ae5faad865a779cec9
hajeong <happykhj67@naver.com>
2023-01-05 07:32:18 +0900

    #19 [ui] detail xml 구현

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.peekabookaos.presentation.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.peekabookaos.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
