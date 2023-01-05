20adbebb3f1606ff48e8dce56919731f0a7fcdb0
Stellarhalo <stellarhalo@naver.com>
2023-01-05 09:09:45 +0900

    #16 [ui] 친구 프로필 클릭시 stroke 생성

--- app/src/main/java/com/sopt/peekabookaos/presentation/bookshelf/BookShelfFriendAdapter.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/bookshelf/BookShelfFriendAdapter.kt
@@ -1,7 +1,11 @@
 package com.sopt.peekabookaos.presentation.bookshelf
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorRes
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.peekabookaos.data.entity.User
@@ -14,7 +18,8 @@ class BookShelfFriendAdapter(
     ListAdapter<User, BookShelfFriendAdapter.FriendProfileViewHolder>(friendDiffUtil) {
 
     private lateinit var itemBookshelfFriendBinding: ItemBookshelfUserProfileBinding
-
+    private var selectPosition = -1
+    private var prePosition = -1
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendProfileViewHolder {
         itemBookshelfFriendBinding =
             ItemBookshelfUserProfileBinding.inflate(
@@ -25,15 +30,23 @@ class BookShelfFriendAdapter(
         return FriendProfileViewHolder(itemBookshelfFriendBinding)
     }
 
-    override fun onBindViewHolder(holder: FriendProfileViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FriendProfileViewHolder, @SuppressLint("RecyclerView") position: Int) {
         holder.onBind(getItem(position))
+        if(selectPosition == position){
+            holder.binding.ivItemBookshelfFriendProfileRedline.visibility = View.VISIBLE
+        }else{
+            holder.binding.ivItemBookshelfFriendProfileRedline.visibility = View.INVISIBLE
+        }
         itemBookshelfFriendBinding.root.setOnClickListener {
             clickListener.onClick(position, getItem(position))
-
+            prePosition = selectPosition
+            selectPosition = position
+            notifyItemChanged(prePosition)
+            notifyItemChanged(selectPosition)
         }
     }
 
-    class FriendProfileViewHolder(private val binding: ItemBookshelfUserProfileBinding) :
+    class FriendProfileViewHolder(val binding: ItemBookshelfUserProfileBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: User) {
             binding.data = data
