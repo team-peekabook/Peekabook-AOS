ecb2da88dfac43f49370bd9fa18aea16f3021133
이빵주 <yungju9982@naver.com>
2023-01-05 17:33:14 +0900

    #13 [fix] postBarcode 함수에서 코루틴 분기처리

--- app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
@@ -1,10 +1,12 @@
 package com.sopt.peekabookaos.presentation.barcodeScanner
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.util.extensions.BarcodeState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class BarcodeViewModel : ViewModel() {
     private val _uiState = MutableStateFlow<BarcodeState>(BarcodeState.Default)
@@ -13,7 +15,15 @@ class BarcodeViewModel : ViewModel() {
     /* 서버 통신 더미 변수 (추후 제거 예정) */
     private val serverStatus = true
 
+    fun postBarcode(barcodeString: String) {
         /* 서버 통신 시 구현 예정*/
-        _uiState.value = true
+        viewModelScope.launch {
+            _uiState.emit(BarcodeState.Default)
+            if (serverStatus) {
+                _uiState.emit(BarcodeState.Success)
+            } else {
+                _uiState.emit(BarcodeState.Fail)
+            }
+        }
     }
 }
