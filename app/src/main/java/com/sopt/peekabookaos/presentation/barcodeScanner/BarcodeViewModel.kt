9610ca29c6ed53f817e01383405eae12feaba18c
이빵주 <yungju9982@naver.com>
2023-01-05 17:31:27 +0900

    #13 [fix] uiState 타입 Boolean에서 BarcodeState 객체로 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
@@ -1,14 +1,15 @@
 package com.sopt.peekabookaos.presentation.barcodeScanner
 
 import androidx.lifecycle.ViewModel
+import com.sopt.peekabookaos.util.extensions.BarcodeState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class BarcodeViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow<BarcodeState>(BarcodeState.Default)
+    val uiState: StateFlow<BarcodeState> = _uiState.asStateFlow()
 
-    private val _uiState = MutableStateFlow(false)
-    val uiState: StateFlow<Boolean> = _uiState.asStateFlow()
 
         /* 서버 통신 시 구현 예정*/
         _uiState.value = true
