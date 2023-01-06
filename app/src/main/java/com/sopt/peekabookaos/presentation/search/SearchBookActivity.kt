07b27e618e69e8ea64ffc9c5189acb6691d353d0
이빵주 <yungju9982@naver.com>
2023-01-07 05:54:50 +0900

    #27 [add] SearchBookActivity 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/SearchBookActivity.kt
@@ -0,0 +1,15 @@
+package com.sopt.peekabookaos.presentation.search
+
+import android.os.Bundle
+import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.databinding.ActivitySearchBookBinding
+import com.sopt.peekabookaos.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchBookActivity :
+    BindingActivity<ActivitySearchBookBinding>(R.layout.activity_search_book) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
