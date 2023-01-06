9a263e560c2412dce2b2c82287a1bef9442209eb
이빵주 <yungju9982@naver.com>
2023-01-07 04:27:35 +0900

    #13 [fix] UiState 확장함수 적용

--- app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
@@ -2,15 +2,16 @@ package com.sopt.peekabookaos.presentation.barcodeScanner
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sopt.peekabookaos.util.extensions.BarcodeState
+import com.sopt.peekabookaos.data.entity.Book
+import com.sopt.peekabookaos.util.extensions.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class BarcodeViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow<BarcodeState>(BarcodeState.Default)
-    val uiState: StateFlow<BarcodeState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow<UiState<Book>>(UiState.IDLE)
+    val uiState: StateFlow<UiState<Book>> = _uiState.asStateFlow()
 
     /* 서버 통신 더미 변수 (추후 제거 예정) */
     private val serverStatus = true
@@ -18,11 +19,11 @@ class BarcodeViewModel : ViewModel() {
     fun postBarcode(barcodeString: String) {
         /* 서버 통신 시 구현 예정*/
         viewModelScope.launch {
-            _uiState.emit(BarcodeState.Default)
+            _uiState.emit(UiState.IDLE)
             if (serverStatus) {
-                _uiState.emit(BarcodeState.Success)
+                _uiState.emit(UiState.Success<Book>(Book()))
             } else {
-                _uiState.emit(BarcodeState.Fail)
+                _uiState.emit(UiState.Error(Throwable()))
             }
         }
     }
