b81b57e03e2409bc0fea15be385249b4d27f610a
이빵주 <yungju9982@naver.com>
2023-01-07 04:25:22 +0900

    #13 [delete] BarcodeState 제거

--- app/src/main/java/com/sopt/peekabookaos/util/extensions/BarcodeState.kt
+++
@@ -1,7 +0,0 @@
-package com.sopt.peekabookaos.util.extensions
-
-sealed class BarcodeState {
-    object Default : BarcodeState()
-    object Success : BarcodeState()
-    object Fail : BarcodeState()
-}
