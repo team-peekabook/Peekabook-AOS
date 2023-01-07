e7440e81adbac67d17d9151b9c11f406fa8bf12c
이빵주 <yungju9982@naver.com>
2023-01-07 09:13:07 +0900

    #27 [add] 검색버튼 클릭리스터 생성

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/SearchBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/SearchBookViewModel.kt
@@ -1,11 +1,13 @@
 package com.sopt.peekabookaos.presentation.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.Book
 import com.sopt.peekabookaos.util.extensions.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class SearchBookViewModel : ViewModel() {
     private val _uiState = MutableStateFlow<UiState<List<Book>>>(UiState.IDLE)
@@ -15,6 +17,20 @@ class SearchBookViewModel : ViewModel() {
     private val serverStatus = true
 
     val bookTitle = MutableStateFlow("")
+
+    fun searchBtnClickListener() {
+        viewModelScope.launch {
+            /* 서버 통신 시 구현 예정*/
+            viewModelScope.launch {
+                _uiState.emit(UiState.IDLE)
+                if (serverStatus) {
+                    _uiState.emit(UiState.Success(dummy))
+                } else {
+                    _uiState.emit(UiState.Error(Throwable()))
+                }
+            }
+        }
+    }
 
     companion object {
         private val dummy = listOf(
