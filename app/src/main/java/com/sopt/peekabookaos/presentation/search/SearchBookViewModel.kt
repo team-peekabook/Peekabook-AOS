47bde40c0009057428b34ba30c4c9e4d6ee2dcff
이빵주 <yungju9982@naver.com>
2023-01-07 09:11:29 +0900

    #27 [add] uiState 변수 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/SearchBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/SearchBookViewModel.kt
@@ -1,4 +1,14 @@
+package com.sopt.peekabookaos.presentation.search
+
 import androidx.lifecycle.ViewModel
+import com.sopt.peekabookaos.data.entity.Book
+import com.sopt.peekabookaos.util.extensions.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SearchBookViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow<UiState<List<Book>>>(UiState.IDLE)
+    val uiState: StateFlow<UiState<List<Book>>> = _uiState.asStateFlow()
+
 }
