5bb7a6d3a827f3049fcc06efb519b786fc165912
이빵주 <yungju9982@naver.com>
2023-01-08 14:13:40 +0900

    #31 [add] SearchUser 액티비티 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.peekabookaos.presentation.search.user
+
+import android.os.Bundle
+import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.databinding.ActivitySearchBookBinding
+import com.sopt.peekabookaos.util.binding.BindingActivity
+
+class SearchUserActivity :
+    BindingActivity<ActivitySearchBookBinding>(R.layout.activity_search_user) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
