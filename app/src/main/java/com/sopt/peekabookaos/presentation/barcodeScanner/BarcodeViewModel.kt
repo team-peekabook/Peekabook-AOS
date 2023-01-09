7785aa18ea631f3fc1536709b39d57e20149423d
이빵주 <yungju9982@naver.com>
2023-01-09 21:45:44 +0900

    #31 [fix] uiState 타입 Book으로 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeViewModel.kt
@@ -3,15 +3,13 @@ package com.sopt.peekabookaos.presentation.barcodeScanner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.Book
-import com.sopt.peekabookaos.util.extensions.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class BarcodeViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow<UiState<Book>>(UiState.IDLE)
-    val uiState: StateFlow<UiState<Book>> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(Book())
+    val uiState = _uiState.asStateFlow()
 
     /* 서버 통신 더미 변수 (추후 제거 예정) */
     private val serverStatus = true
