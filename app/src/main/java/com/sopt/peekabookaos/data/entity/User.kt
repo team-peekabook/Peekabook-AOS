5f196de275e550a5c0dabec98be09c7f66c12e3e
이빵주 <yungju9982@naver.com>
2023-01-10 01:29:57 +0900

    #31 [add] User 초기값 entity에 설정

--- app/src/main/java/com/sopt/peekabookaos/data/entity/User.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/User.kt
@@ -1,9 +1,9 @@
 package com.sopt.peekabookaos.data.entity
 
 data class User(
-    val id: Int,
-    val nickname: String,
-    val profileImage: String,
-    val intro: String,
-    val isFollowed: Boolean
+    val id: Int = -1,
+    val nickname: String = "",
+    val profileImage: String = "",
+    val intro: String = "",
+    val isFollowed: Boolean = false
 )
