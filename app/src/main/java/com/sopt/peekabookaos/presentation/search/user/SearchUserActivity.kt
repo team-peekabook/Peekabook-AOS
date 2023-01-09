2d73693e58feda4cea82d3e573be6919cc0dd07f
이빵주 <yungju9982@naver.com>
2023-01-09 17:14:55 +0900

    #31 [feat] SearchUserActivity 영역 클릭시에 키보드 숨기기 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
@@ -1,10 +1,12 @@
 package com.sopt.peekabookaos.presentation.search.user
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.sopt.peekabookaos.R
-import com.sopt.peekabookaos.databinding.ActivitySearchBookBinding
+import com.sopt.peekabookaos.databinding.ActivitySearchUserBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
+import com.sopt.peekabookaos.util.extensions.KeyBoardUtil
 
 @AndroidEntryPoint
 class SearchUserActivity :
@@ -14,5 +16,19 @@ class SearchUserActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = searchUserViewModel
+        initEditTextClearFocus()
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initEditTextClearFocus() {
+        binding.clSearchUser.setOnTouchListener { _, _ ->
+            KeyBoardUtil.hide(activity = this)
+            return@setOnTouchListener false
+        }
+
+        binding.btnSearchUser.setOnTouchListener { _, _ ->
+            KeyBoardUtil.hide(activity = this)
+            return@setOnTouchListener false
+        }
+    }
+
     }
 }
