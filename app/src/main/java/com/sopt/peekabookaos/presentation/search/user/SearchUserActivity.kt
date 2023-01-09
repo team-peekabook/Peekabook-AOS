0fa643c980cdc274dc1609e155e259b10cc478d2
이빵주 <yungju9982@naver.com>
2023-01-09 17:10:37 +0900

    #31 [add] SearchUserActivity 데이터바인딩 초기화

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
@@ -1,6 +1,7 @@
 package com.sopt.peekabookaos.presentation.search.user
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivitySearchBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
@@ -8,6 +9,8 @@ import com.sopt.peekabookaos.util.binding.BindingActivity
 @AndroidEntryPoint
 class SearchUserActivity :
     BindingActivity<ActivitySearchUserBinding>(R.layout.activity_search_user) {
+    private val searchUserViewModel: SearchUserViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
