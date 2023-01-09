446db71acc760f57cf61d84d1a0f44eeafa8aae2
이빵주 <yungju9982@naver.com>
2023-01-09 17:09:54 +0900

    #31 [add] SearchUserActivity @AndroidEntryPoint 어노테이션 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserActivity.kt
@@ -5,6 +5,7 @@ import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivitySearchBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
 
+@AndroidEntryPoint
 class SearchUserActivity :
     BindingActivity<ActivitySearchBookBinding>(R.layout.activity_search_user) {
     override fun onCreate(savedInstanceState: Bundle?) {
