944b0d49c98f607c128ac09cf5ff74c6285083c6
이빵주 <yungju9982@naver.com>
2023-01-10 08:31:20 +0900

    #14 [add] providesRecommendService 생성

--- app/src/main/java/com/sopt/peekabookaos/di/RetrofitServiceModule.kt
+++ app/src/main/java/com/sopt/peekabookaos/di/RetrofitServiceModule.kt
@@ -2,6 +2,7 @@ package com.sopt.peekabookaos.di
 
 import com.sopt.peekabookaos.data.service.DetailService
 import com.sopt.peekabookaos.data.service.NotificationService
+import com.sopt.peekabookaos.data.service.RecommendService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,4 +19,8 @@ object RetrofitServiceModule {
     @Provides
     fun providesNotificationService(retrofit: Retrofit): NotificationService =
         retrofit.create(NotificationService::class.java)
+
+    @Provides
+    fun providesRecommendService(retrofit: Retrofit): RecommendService =
+        retrofit.create(RecommendService::class.java)
 
