3e0f0f4d3e9616492e7519f8e6d874163dc55719
이빵주 <yungju9982@naver.com>
2023-01-09 21:56:09 +0900

    #31 [fix] UiState 쓰던거 Book으로 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/book/SearchBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/book/SearchBookViewModel.kt
@@ -3,15 +3,16 @@ package com.sopt.peekabookaos.presentation.search.book
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.Book
-import com.sopt.peekabookaos.util.extensions.UiState
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SearchBookViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow<UiState<List<Book>>>(UiState.IDLE)
-    val uiState: StateFlow<UiState<List<Book>>> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(emptyList<Book>())
+    val uiState = _uiState.asStateFlow()
+
 
     /* 서버 통신 시 제거 예정 */
     private val serverStatus = true
