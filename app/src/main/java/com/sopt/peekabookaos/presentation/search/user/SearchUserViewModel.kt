119f778ed277e8741ba36d006dd455b0297bb813
이빵주 <yungju9982@naver.com>
2023-01-09 17:17:39 +0900

    #31 [add] SearchUser뷰모델 isServerStatus 변수 설정

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserViewModel.kt
@@ -1,10 +1,15 @@
 package com.sopt.peekabookaos.presentation.search.user
 
 import com.sopt.peekabookaos.data.entity.User
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 class SearchUserViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(SearchUserUiState())
     val uiState = _uiState.asStateFlow()
+
+    private val _isServerStatus = MutableSharedFlow<Boolean>()
+    val isServerStatus = _isServerStatus.asSharedFlow()
 
 }
