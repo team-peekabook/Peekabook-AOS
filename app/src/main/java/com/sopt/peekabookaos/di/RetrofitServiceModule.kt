fe25124aeefde5988067025e0e9838e89482c723
이빵주 <yungju9982@naver.com>
2023-01-10 08:30:41 +0900

    #14 [add] providesDetailService 생성

--- app/src/main/java/com/sopt/peekabookaos/di/RetrofitServiceModule.kt
+++ app/src/main/java/com/sopt/peekabookaos/di/RetrofitServiceModule.kt
@@ -1,9 +1,16 @@
 package com.sopt.peekabookaos.di
 
+import com.sopt.peekabookaos.data.service.DetailService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitServiceModule
+object RetrofitServiceModule {
+    @Provides
+    fun providesDetailService(retrofit: Retrofit): DetailService =
+        retrofit.create(DetailService::class.java)
+
