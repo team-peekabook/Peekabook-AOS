abf675b37346a88d4e5b786222ed25d6fd3d8d44
이빵주 <yungju9982@naver.com>
2023-01-09 17:17:23 +0900

    #31 [add] SearchUser뷰모델 uiState 변수 설정

--- app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/search/user/SearchUserViewModel.kt
@@ -1,4 +1,10 @@
 package com.sopt.peekabookaos.presentation.search.user
 
+import com.sopt.peekabookaos.data.entity.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 class SearchUserViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(SearchUserUiState())
+    val uiState = _uiState.asStateFlow()
+
 }
