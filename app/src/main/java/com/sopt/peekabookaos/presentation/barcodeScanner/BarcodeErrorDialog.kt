14e3f029d8a893ee144d9845837c38318b5801eb
이빵주 <yungju9982@naver.com>
2023-01-10 06:20:06 +0900

    #30 [add] 에러다이얼로그 프래그먼트 구현

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/barcodeScanner/BarcodeErrorDialog.kt
@@ -0,0 +1,54 @@
+package com.sopt.peekabookaos.presentation.barcodeScanner
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.databinding.DialogBarcodeErrorBinding
+
+class BarcodeErrorDialog : DialogFragment() {
+    private var _binding: DialogBarcodeErrorBinding? = null
+    private val binding get() = _binding ?: error(getString(R.string.binding_error))
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogBarcodeErrorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initLayout()
+        initCloseBtnClickListener()
+    }
+
+    private fun initLayout() {
+        val ratio = 0.89
+        val layoutParams = requireNotNull(dialog).window!!.attributes
+        layoutParams.width = (resources.displayMetrics.widthPixels * ratio).toInt()
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
+        requireNotNull(dialog).window!!.attributes = layoutParams
+        isCancelable = true
+    }
+
+    private fun initCloseBtnClickListener() {
+        binding.btnBarcodeErrorClose.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "BarcodeErrorDialogFragment"
+    }
+}
