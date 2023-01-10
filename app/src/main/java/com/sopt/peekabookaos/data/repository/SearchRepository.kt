4af7f4d57289a2485d56c03b8e8c97831978d952
이빵주 <yungju9982@naver.com>
2023-01-10 16:47:58 +0900

    #42 [fix] getSearchUser 레포지토리 수정

--- app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepository.kt
@@ -1,8 +1,7 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.BaseResponse
 import com.sopt.peekabookaos.data.entity.User
 
 interface SearchRepository {
-    suspend fun getSearchUser(nickname: String): Result<BaseResponse<List<User>>>
+    suspend fun getSearchUser(nickname: String): Result<User>
 }
