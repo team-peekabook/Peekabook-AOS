666d46360ecc57a180d93db9cc519f54d68b7af2
Kim hajeong <happykhj67@gmail.com>
2023-01-11 03:43:01 +0900

    #55 [add] recommendedBook 라이브데이터 변수 생성

--- app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendViewModel.kt
@@ -1,6 +1,16 @@
+package com.sopt.peekabookaos.presentation.recommend
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sopt.peekabookaos.data.entity.Recommend
 import com.sopt.peekabookaos.data.repository.RecommendRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
 @HiltViewModel
 class RecommendViewModel @Inject constructor(
     private val recommendRepository: RecommendRepository
 ) : ViewModel() {
+    private val _recommendedBook = MutableLiveData<List<Recommend>>()
+    val recommendedBook: LiveData<List<Recommend>> = _recommendedBook
