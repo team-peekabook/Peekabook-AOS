a7b3a1112c0cf1c4e16da88a3c51ec9bc30fccc3
이빵주 <yungju9982@naver.com>
2023-01-10 14:00:41 +0900

    #42 [add] getSearchUser 레포지토리impl 생성

--- app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepositoryImpl.kt
@@ -1,8 +1,13 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.User
 import com.sopt.peekabookaos.data.source.remote.SearchDataSource
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(
     private val searchDataSource: SearchDataSource
-) : SearchRepository
+) : SearchRepository {
+    override suspend fun getSearchUser(nickname: String): Result<BaseResponse<List<User>>> =
+        kotlin.runCatching { searchDataSource.getSearchUser(nickname) }
+}
