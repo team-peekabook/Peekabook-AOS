18afae3572c6d85addb2bfaa5fbb101523ee3098
이빵주 <yungju9982@naver.com>
2023-01-10 13:58:10 +0900

    #42 [add] providesSearchRepository 생성

--- app/src/main/java/com/sopt/peekabookaos/di/RepositoryModule.kt
+++ app/src/main/java/com/sopt/peekabookaos/di/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.sopt.peekabookaos.di
 
+import com.sopt.peekabookaos.data.repository.SearchRepository
+import com.sopt.peekabookaos.data.repository.SearchRepositoryImpl
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,3 +9,10 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun providesSearchRepository(
+        searchRepositoryImpl: SearchRepositoryImpl
+    ): SearchRepository = searchRepositoryImpl
+
