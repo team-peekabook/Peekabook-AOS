2d089c9103216869211523ed8ca64f0423f5da16
이빵주 <yungju9982@naver.com>
2023-01-10 22:23:02 +0900

    #50 [feat] 책등록하기 서버 연결 함수 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookViewModel.kt
@@ -3,10 +3,14 @@ package com.sopt.peekabookaos.presentation.createUpdateBook
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.Book
+import com.sopt.peekabookaos.data.entity.request.CreateBookRequest
 import com.sopt.peekabookaos.data.repository.CreateUpdateRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltViewModel
 class CreateUpdateBookViewModel @Inject constructor(
@@ -35,7 +39,25 @@ class CreateUpdateBookViewModel @Inject constructor(
         if (_isUpdateView.value == true) {
             Timber.d("책 수정 서버 통신")
         } else {
-            Timber.d("책 등록 서버 통신")
+            postCreateBook()
+        }
+    }
+
+    private fun postCreateBook() {
+        initCreateBookRequest(
+            bookImage = _bookData.value!!.bookImage,
+            bookTitle = _bookData.value!!.bookTitle,
+            author = _bookData.value!!.author,
+            description = comment.value,
+            memo = memo.value
+        )
+        viewModelScope.launch {
+            createUpdateRepository.postCreateBook(createUpdateRequest.value!!)
+                .onSuccess {
+                    _isServerStatus.value = true
+                }.onFailure {
+                    _isServerStatus.value = false
+                }
         }
     }
 
