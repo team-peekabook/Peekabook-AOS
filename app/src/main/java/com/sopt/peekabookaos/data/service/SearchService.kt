251c90528cc5288c015d0b7643429417bbb85382
이빵주 <yungju9982@naver.com>
2023-01-10 16:47:26 +0900

    #42 [fix] getSearchUser 서비스 수정

--- app/src/main/java/com/sopt/peekabookaos/data/service/SearchService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/SearchService.kt
@@ -1,13 +1,13 @@
 package com.sopt.peekabookaos.data.service
 
 import com.sopt.peekabookaos.data.entity.BaseResponse
-import com.sopt.peekabookaos.data.entity.User
+import com.sopt.peekabookaos.data.entity.response.SearchUserResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SearchService {
     @GET("friend?nickname=")
     suspend fun getSearchUser(
-        @Path("nickname") nickname: String
-    ): BaseResponse<List<User>>
+        @Query("nickname") nickname: String
+    ): BaseResponse<List<SearchUserResponse>>
 }
