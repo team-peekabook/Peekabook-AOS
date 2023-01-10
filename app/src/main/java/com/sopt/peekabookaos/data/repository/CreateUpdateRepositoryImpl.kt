1efee2e45be4d0218d7d18d45332cc89c67e00de
이빵주 <yungju9982@naver.com>
2023-01-10 22:20:40 +0900

    #50 [add] postCreateBook 레포지토리impl 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepositoryImpl.kt
@@ -1,8 +1,15 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.request.CreateBookRequest
+import com.sopt.peekabookaos.data.entity.response.CreateBookResponse
 import com.sopt.peekabookaos.data.source.remote.CreateUpdateDataSource
 import javax.inject.Inject
 
 class CreateUpdateRepositoryImpl @Inject constructor(
     private val createUpdateDataSource: CreateUpdateDataSource
-) : CreateUpdateRepository
+) : CreateUpdateRepository {
+    override suspend fun postCreateBook(createBookRequest: CreateBookRequest): Result<CreateBookResponse> =
+        kotlin.runCatching {createUpdateDataSource.postCreateBook(createBookRequest)}.map { response ->
+            response.data!!
+        }
+}
