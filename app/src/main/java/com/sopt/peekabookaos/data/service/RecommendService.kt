28b5445c396ee3dfefc443a32d0b8d15624394bf
Kim hajeong <happykhj67@gmail.com>
2023-01-11 03:37:30 +0900

    #55 [add] getRecommend함수 서비스에 생성

--- app/src/main/java/com/sopt/peekabookaos/data/service/RecommendService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/RecommendService.kt
@@ -1,3 +1,10 @@
 package com.sopt.peekabookaos.data.service
 
-interface RecommendService
+import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.response.RecommendResponse
+import retrofit2.http.GET
+
+interface RecommendService {
+    @GET("recommend")
+    suspend fun getRecommend(): BaseResponse<RecommendResponse>
+}
