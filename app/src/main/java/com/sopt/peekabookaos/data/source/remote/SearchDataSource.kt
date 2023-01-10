d03407b7972c58c6410a19b0595e3e635a769917
이빵주 <yungju9982@naver.com>
2023-01-10 17:12:55 +0900

    #42 [fix] response 객체 수정

--- app/src/main/java/com/sopt/peekabookaos/data/source/remote/SearchDataSource.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/source/remote/SearchDataSource.kt
@@ -8,6 +8,6 @@ import javax.inject.Inject
 data class SearchDataSource @Inject constructor(
     private val searchService: SearchService
 ) {
-    suspend fun getSearchUser(nickname: String): BaseResponse<List<SearchUserResponse>> =
+    suspend fun getSearchUser(nickname: String): BaseResponse<SearchUserResponse> =
         searchService.getSearchUser(nickname)
 }
