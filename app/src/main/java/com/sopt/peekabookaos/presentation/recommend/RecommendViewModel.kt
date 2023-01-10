42482a40e0ab5b5b9e96a97a9848dd8ee2377906
Kim hajeong <happykhj67@gmail.com>
2023-01-11 03:42:18 +0900

    #55 [add] 뷰모델에 레포지토리 주입

--- app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendViewModel.kt
@@ -1,2 +1,6 @@
+import com.sopt.peekabookaos.data.repository.RecommendRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 @HiltViewModel
+class RecommendViewModel @Inject constructor(
+    private val recommendRepository: RecommendRepository
+) : ViewModel() {
