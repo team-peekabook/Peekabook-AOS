6db4b11d72f1f3c876ca0c9a943a171dc5c3bd6b
이빵주 <yungju9982@naver.com>
2023-01-10 13:59:54 +0900

    #42 [add] getSearchUser Service 생성

--- app/src/main/java/com/sopt/peekabookaos/data/service/SearchService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/SearchService.kt
@@ -1,3 +1,13 @@
 package com.sopt.peekabookaos.data.service
 
-interface SearchService
+import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.User
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface SearchService {
+    @GET("friend?nickname=")
+    suspend fun getSearchUser(
+        @Path("nickname") nickname: String
+    ): BaseResponse<List<User>>
+}
