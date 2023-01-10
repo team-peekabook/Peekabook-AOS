eb362fe2fe4e43ddf6414d20f6650831fe449c4e
Kim hajeong <happykhj67@gmail.com>
2023-01-11 03:43:43 +0900

    #55 [feat] 추천뷰 서버조회 함수 생성

--- app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/recommend/RecommendViewModel.kt
@@ -3,9 +3,12 @@ package com.sopt.peekabookaos.presentation.recommend
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.Recommend
 import com.sopt.peekabookaos.data.repository.RecommendRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,3 +20,15 @@ class RecommendViewModel @Inject constructor(
 
     private val _recommendingBook = MutableLiveData<List<Recommend>>()
     val recommendingBook: LiveData<List<Recommend>> = _recommendingBook
+    private fun getRecommend() {
+        viewModelScope.launch {
+            recommendRepository.getRecommend()
+                .onSuccess { response ->
+                    _recommendingBook.value = response.recommendingBook
+                    _recommendedBook.value = response.recommendedBook
+                }.onFailure { throwable ->
+                    Timber.e("$throwable")
+                }
+        }
+    }
+}
