cc3edb192799368fec6bc2566e4ed65b67fdb1fe
이빵주 <yungju9982@naver.com>
2023-01-10 23:59:02 +0900

    #53 [add] postFollow 레포지토리 생성

--- app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepository.kt
@@ -1,7 +1,10 @@
 package com.sopt.peekabookaos.data.repository
 
 import com.sopt.peekabookaos.data.entity.User
+import com.sopt.peekabookaos.data.entity.response.FollowResponse
 
 interface SearchRepository {
     suspend fun getSearchUser(nickname: String): Result<User>
+
+    suspend fun postFollow(friendId: String): Result<FollowResponse>
 }
