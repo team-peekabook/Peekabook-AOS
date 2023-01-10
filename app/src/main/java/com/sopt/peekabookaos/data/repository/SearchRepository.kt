703f8481462dc101e22bdb34488cd7a81f52256f
이빵주 <yungju9982@naver.com>
2023-01-10 14:00:23 +0900

    #42 [add] getSearchUser 레포지토리 생성

--- app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/SearchRepository.kt
@@ -1,3 +1,8 @@
 package com.sopt.peekabookaos.data.repository
 
-interface SearchRepository
+import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.User
+
+interface SearchRepository {
+    suspend fun getSearchUser(nickname: String): Result<BaseResponse<List<User>>>
+}
