e5985112369935f49cc952f2281015466dcff1fa
이빵주 <yungju9982@naver.com>
2023-01-11 20:34:58 +0900

    #67 [add] WarningDialogContent 데이터클래스 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/util/dialog/WarningDialogContent.kt
@@ -0,0 +1,31 @@
+package com.sopt.peekabookaos.util.dialog
+
+import android.content.Context
+import com.sopt.peekabookaos.R
+
+data class WarningDialogContent(
+    val title: String = "",
+    val cancel: String = "",
+    val confirm: String = ""
+) {
+    fun getWarningDeleteBook(context: Context): WarningDialogContent =
+        WarningDialogContent(
+            title = context.getString(R.string.warning_dialog_delete_book_title),
+            cancel = context.getString(R.string.warning_dialog_cancel),
+            confirm = context.getString(R.string.warning_dialog_delete)
+        )
+
+    fun getWarningRecommendBook(context: Context, follower: String): WarningDialogContent =
+        WarningDialogContent(
+            title = context.getString(R.string.warning_dialog_recommend_title, follower),
+            cancel = context.getString(R.string.warning_dialog_cancel),
+            confirm = context.getString(R.string.warning_dialog_recommend)
+        )
+
+    fun getWarningDeleteFollow(context: Context, follower: String): WarningDialogContent =
+        WarningDialogContent(
+            title = context.getString(R.string.warning_dialog_delete_follower_title, follower),
+            cancel = context.getString(R.string.warning_dialog_cancel),
+            confirm = context.getString(R.string.warning_dialog_delete)
+        )
+}
