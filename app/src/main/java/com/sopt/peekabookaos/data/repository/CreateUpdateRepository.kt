d0938f5df7123e6691cda86a9161610e5937040f
이빵주 <yungju9982@naver.com>
2023-01-12 00:25:42 +0900

    #71 [add] deleteBook 래포지토리 생성

--- app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepository.kt
@@ -1,8 +1,11 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.NoResponse
 import com.sopt.peekabookaos.data.entity.request.CreateBookRequest
 import com.sopt.peekabookaos.data.entity.response.CreateBookResponse
 
 interface CreateUpdateRepository {
     suspend fun postCreateBook(createBookRequest: CreateBookRequest): Result<CreateBookResponse>
+
+    suspend fun deleteBook(bookId: Int): Result<NoResponse>
 }
