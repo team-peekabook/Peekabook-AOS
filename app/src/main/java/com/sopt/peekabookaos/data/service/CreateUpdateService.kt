2077ef3e887b5759ca8c80e00ac7e93c36c054ef
이빵주 <yungju9982@naver.com>
2023-01-12 00:22:54 +0900

    #71 [add] deleteBookId 서비스 추가

--- app/src/main/java/com/sopt/peekabookaos/data/service/CreateUpdateService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/CreateUpdateService.kt
@@ -1,14 +1,22 @@
 package com.sopt.peekabookaos.data.service
 
 import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.NoResponse
 import com.sopt.peekabookaos.data.entity.request.CreateBookRequest
 import com.sopt.peekabookaos.data.entity.response.CreateBookResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface CreateUpdateService {
     @POST("bookshelf")
     suspend fun postCreateBook(
         @Body body: CreateBookRequest
     ): BaseResponse<CreateBookResponse>
+
+    @DELETE("bookshelf/{bookId}")
+    suspend fun deleteBook(
+        @Path("bookId") bookId: Int
+    ): NoResponse
 }
