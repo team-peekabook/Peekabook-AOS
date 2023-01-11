8722b79036ace6112f30726c2fff99eda2c2f6e5
Kim hajeong <happykhj67@gmail.com>
2023-01-11 16:13:44 +0900

    #57 [add] serialName 추가

--- app/src/main/java/com/sopt/peekabookaos/data/entity/Detail.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/Detail.kt
@@ -1,10 +1,12 @@
 package com.sopt.peekabookaos.data.entity
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Detail(
     val description: String,
     val memo: String,
+    @SerialName("Book")
     val book: Book
 )
--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/DetailResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.sopt.peekabookaos.data.entity.response
-
-import com.sopt.peekabookaos.data.entity.Detail
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DetailResponse(
-    val detailBook: List<Detail>
-)
