dd19fac6ad44b120f0fe75e94d438d2949283c23
이빵주 <yungju9982@naver.com>
2023-01-11 17:27:25 +0900

    #53 [fix] FollowResponse에 Serializable 어노테이션 추가

--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/FollowResponse.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/response/FollowResponse.kt
@@ -1,5 +1,8 @@
 package com.sopt.peekabookaos.data.entity.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FollowResponse(
     val followId: Int,
     val receiverId: Int,
