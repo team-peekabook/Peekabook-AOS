e6d62650057982ab28888974cdd595e62a2da07a
Kim hajeong <happykhj67@gmail.com>
2023-01-11 15:04:43 +0900

    #57 [add] getDetail 함수 데이터소스에 추가

--- app/src/main/java/com/sopt/peekabookaos/data/source/remote/DetailDataSource.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/source/remote/DetailDataSource.kt
@@ -1,8 +1,14 @@
 package com.sopt.peekabookaos.data.source.remote
 
+import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import com.sopt.peekabookaos.data.entity.response.RecommendResponse
 import com.sopt.peekabookaos.data.service.DetailService
 import javax.inject.Inject
 
 data class DetailDataSource @Inject constructor(
     private val detailService: DetailService
-)
+) {
+    suspend fun getDetail(): BaseResponse<DetailResponse> =
+        detailService.getDetail()
+}
