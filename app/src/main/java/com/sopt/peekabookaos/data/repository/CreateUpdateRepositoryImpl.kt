dc9b49d20c44e88fa644711fa8c99e3701ed9c1f
이빵주 <yungju9982@naver.com>
2023-01-12 00:25:52 +0900

    #71 [add] deleteBook 레포지토리 impl 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.NoResponse
 import com.sopt.peekabookaos.data.entity.request.CreateBookRequest
 import com.sopt.peekabookaos.data.entity.response.CreateBookResponse
 import com.sopt.peekabookaos.data.source.remote.CreateUpdateDataSource
@@ -13,4 +14,7 @@ class CreateUpdateRepositoryImpl @Inject constructor(
             .map { response ->
                 response.data!!
             }
+
+    override suspend fun deleteBook(bookId: Int): Result<NoResponse> =
+        kotlin.runCatching { createUpdateDataSource.deleteBook(bookId) }
 }
