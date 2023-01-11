5e9ec5e968dc767ddd622ca168010f5a2102778c
Kim hajeong <happykhj67@gmail.com>
2023-01-12 03:23:05 +0900

    #64 [fix] 데이터클래스명 수정

--- app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepository.kt
@@ -1,7 +1,10 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.Detail
+import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import com.sopt.peekabookaos.data.entity.NoResponse
 
 interface DetailRepository {
-    suspend fun getDetail(bookId: Int): Result<Detail>
+    suspend fun getDetail(bookId: Int): Result<DetailResponse>
+
+    suspend fun deleteDetail(bookId: Int): Result<NoResponse>
 }
--- app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepositoryImpl.kt
@@ -1,14 +1,17 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.Detail
+import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import com.sopt.peekabookaos.data.entity.NoResponse
 import com.sopt.peekabookaos.data.source.remote.DetailDataSource
 import javax.inject.Inject
 
 class DetailRepositoryImpl @Inject constructor(
     private val detailDataSource: DetailDataSource
 ) : DetailRepository {
-    override suspend fun getDetail(bookId: Int): Result<Detail> =
+    override suspend fun getDetail(bookId: Int): Result<DetailResponse> =
         kotlin.runCatching { detailDataSource.getDetail(bookId) }.map { response ->
             response.data!!
         }
+    override suspend fun deleteDetail(bookId: Int): Result<NoResponse> =
+        kotlin.runCatching { detailDataSource.deleteDetail(bookId) }
 }
