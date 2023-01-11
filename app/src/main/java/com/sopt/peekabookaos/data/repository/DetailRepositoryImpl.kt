cdb3732ce3d51097d60e22833a1ff24436a9742d
Kim hajeong <happykhj67@gmail.com>
2023-01-11 16:20:37 +0900

    #57 [fix] DetailActivity 수정

--- app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepositoryImpl.kt
@@ -1,14 +1,14 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import com.sopt.peekabookaos.data.entity.Detail
 import com.sopt.peekabookaos.data.source.remote.DetailDataSource
 import javax.inject.Inject
 
 class DetailRepositoryImpl @Inject constructor(
     private val detailDataSource: DetailDataSource
 ) : DetailRepository {
-    override suspend fun getDetail(): Result<DetailResponse> =
-        kotlin.runCatching { detailDataSource.getDetail() }.map { response ->
+    override suspend fun getDetail(bookId: Int): Result<Detail> =
+        kotlin.runCatching { detailDataSource.getDetail(bookId) }.map { response ->
             response.data!!
         }
 }
