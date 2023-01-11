ed406248a92c1986a20bb24cf3b4592fb61cb572
Kim hajeong <happykhj67@gmail.com>
2023-01-11 15:03:54 +0900

    #57 [add] getDetail 함수 데이터소스에 추가

--- app/src/main/java/com/sopt/peekabookaos/data/service/DetailService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/DetailService.kt
@@ -1,3 +1,10 @@
 package com.sopt.peekabookaos.data.service
 
-interface DetailService
+import com.sopt.peekabookaos.data.entity.BaseResponse
+import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import retrofit2.http.GET
+
+interface DetailService {
+    @GET("detail")
+    suspend fun getDetail(): BaseResponse<DetailResponse>
+}
