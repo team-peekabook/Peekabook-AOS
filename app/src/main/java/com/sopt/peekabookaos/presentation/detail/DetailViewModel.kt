166c0e223ec67d1e9b5e840aec7b96d7ee257e91
Kim hajeong <happykhj67@gmail.com>
2023-01-11 15:05:52 +0900

    #57 [fix] 뷰모델 변경

--- app/src/main/java/com/sopt/peekabookaos/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/detail/DetailViewModel.kt
@@ -3,12 +3,20 @@ package com.sopt.peekabookaos.presentation.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.sopt.peekabookaos.data.entity.Book
+import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.Detail
+import com.sopt.peekabookaos.data.repository.DetailRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class DetailViewModel : ViewModel() {
-    private val _detailData = MutableLiveData<Detail>()
-    val detailData: LiveData<Detail> = _detailData
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val detailRepository: DetailRepository
+) : ViewModel() {
+    private val _detailData = MutableLiveData<List<Detail>>()
+    val detailData: LiveData<List<Detail>> = _detailData
 
     private val _isMyDetailView = MutableLiveData<Boolean>()
     val isMyDetailView: LiveData<Boolean> = _isMyDetailView
@@ -16,8 +24,7 @@ class DetailViewModel : ViewModel() {
     private val bookId = MutableLiveData<Int>()
 
     init {
-        initDetailMyData()
-        initDetailFriendData()
+        getDetail()
     }
 
     fun initIsMyDetailView(detail: Boolean) {
@@ -28,27 +35,14 @@ class DetailViewModel : ViewModel() {
         bookId.value = id
     }
 
-    fun initDetailMyData() {
-        _detailData.value = Detail(
-            description = "안드로이드 스튜디오에서 구현되는 다이얼로그에 대해 알아봅시다. 구글에서 완전히 열심히 찾아보면 갤럭시나 아이폰을 사용하면 다이얼로그를 잘 알것이다. 어떤 버튼을 클릭했을 때나 이벤트가 실행되거나 완료되면 어떤 확인문구와 함께 작게 뜨는 창을 본적이 있을 것이다. 그것이 다이얼로그다. 다이얼로그에는 종류가 정말 많은데 코드가 어떻게 구현되는지 알아보도록 하자",
-            memo = "재밌다!",
-            book = Book(
-                bookImage = "http://image.yes24.com/goods/90365124/XL",
-                bookTitle = "눈보라 체이스",
-                author = "양윤옥"
-            )
-        )
-    }
-
-    fun initDetailFriendData() {
-        _detailData.value = Detail(
-            description = "구글에서 완전히 열심히 찾아보면 갤럭시나 아이폰을 사용하면 다이얼로그를 잘 알것이다. 어떤 버튼을 클릭했을 때나 이벤트가 실행되거나 완료되면 어떤 확인문구와 함께 작게 뜨는 창을 본적이 있을 것이다. 그것이 다이얼로그다. 다이얼로그에는 종류가 정말 많은데 코드가 어떻게 구현되는지 알아보도록 하자",
-            memo = "",
-            book = Book(
-                bookImage = "http://image.yes24.com/goods/90365124/XL",
-                bookTitle = "아무튼, 여름",
-                author = "김신회"
-            )
-        )
+    private fun getDetail() {
+        viewModelScope.launch {
+            detailRepository.getDetail()
+                .onSuccess { response ->
+                    _detailData.value = response.detailBook
+                }.onFailure { throwable ->
+                    Timber.e("$throwable")
+                }
+        }
     }
 }
