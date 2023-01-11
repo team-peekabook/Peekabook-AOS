2b4b21ad89455b7ea91410ada16abc7bc3cfbfa9
Kim hajeong <happykhj67@gmail.com>
2023-01-11 15:02:10 +0900

    #57 [add] getDetail 함수 레포지토리impl에 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepositoryImpl.kt
@@ -1,8 +1,14 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.response.DetailResponse
 import com.sopt.peekabookaos.data.source.remote.DetailDataSource
 import javax.inject.Inject
 
 class DetailRepositoryImpl @Inject constructor(
     private val detailDataSource: DetailDataSource
-) : DetailRepository
+) : DetailRepository {
+    override suspend fun getDetail(): Result<DetailResponse> =
+        kotlin.runCatching { detailDataSource.getDetail() }.map { response ->
+            response.data!!
+        }
+}
