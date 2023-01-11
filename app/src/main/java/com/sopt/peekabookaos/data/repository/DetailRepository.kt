aed336e16bf20237c49a0a88ff6b27c33bf02274
Kim hajeong <happykhj67@gmail.com>
2023-01-11 16:16:07 +0900

    #57 [add] getDetail함수에 params 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/DetailRepository.kt
@@ -1,7 +1,7 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import com.sopt.peekabookaos.data.entity.Detail
 
 interface DetailRepository {
-    suspend fun getDetail(): Result<DetailResponse>
+    suspend fun getDetail(bookId: Int): Result<Detail>
 }
--- app/src/main/java/com/sopt/peekabookaos/data/service/DetailService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/DetailService.kt
@@ -1,10 +1,13 @@
 package com.sopt.peekabookaos.data.service
 
 import com.sopt.peekabookaos.data.entity.BaseResponse
-import com.sopt.peekabookaos.data.entity.response.DetailResponse
+import com.sopt.peekabookaos.data.entity.Detail
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DetailService {
-    @GET("detail")
-    suspend fun getDetail(): BaseResponse<DetailResponse>
+    @GET("bookshelf/detail/{bookId}")
+    suspend fun getDetail(
+        @Path("bookId") bookId: Int
+    ): BaseResponse<Detail>
 }
