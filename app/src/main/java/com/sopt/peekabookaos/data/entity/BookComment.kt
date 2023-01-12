34fc08dbecd32296b8899c873550d3a3d19a4b1d
이빵주 <yungju9982@naver.com>
2023-01-12 21:41:17 +0900

    #71 [add] 엔티티 생성

--- app/src/main/java/com/sopt/peekabookaos/data/entity/BookComment.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/BookComment.kt
@@ -2,9 +2,11 @@ package com.sopt.peekabookaos.data.entity
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Parcelize
 data class BookComment(
-    val description: String,
-    val memo: String
+    val description: String = "",
+    val memo: String = ""
 ) : Parcelable
--- app/src/main/java/com/sopt/peekabookaos/data/entity/request/PatchBookRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.sopt.peekabookaos.data.entity.request
-
-data class PatchBookRequest(
-    val description: String,
-    val memo: String
-)
