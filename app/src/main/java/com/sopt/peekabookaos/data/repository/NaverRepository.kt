2d530c5a1a686e4e441ba4223be2dfc00920ae00
이빵주 <yungju9982@naver.com>
2023-01-12 22:19:05 +0900

    #84 [add] getBookToTitle 레포지토리 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/NaverRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/NaverRepository.kt
@@ -3,5 +3,7 @@ package com.sopt.peekabookaos.data.repository
 import com.sopt.peekabookaos.data.entity.Book
 
 interface NaverRepository {
+    suspend fun getBookToTitle(title: String): Result<Book>
+
     suspend fun getBookToBarcode(isbn: String): Result<Book>
 }
