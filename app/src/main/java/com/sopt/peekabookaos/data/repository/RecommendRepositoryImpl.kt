b1f492885a921c10c16d008b6b1c25c10f0b88cd
Kim hajeong <happykhj67@gmail.com>
2023-01-12 18:17:49 +0900

    #79 [add] postRecommend함수 레포지토리Impl에 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/RecommendRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/RecommendRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.request.RecommendationRequest
 import com.sopt.peekabookaos.data.entity.response.RecommendResponse
+import com.sopt.peekabookaos.data.entity.response.RecommendationResponse
 import com.sopt.peekabookaos.data.source.remote.RecommendDataSource
 import javax.inject.Inject
 
@@ -11,4 +13,18 @@ class RecommendRepositoryImpl @Inject constructor(
         kotlin.runCatching { recommendDataSource.getRecommend() }.map { response ->
             response.data!!
         }
+
+    override suspend fun postRecommendation(
+        recommendationRequest: RecommendationRequest,
+        friendId: Int
+    ): Result<RecommendationResponse> =
+        kotlin.runCatching {
+            recommendDataSource.postRecommendation(
+                recommendationRequest,
+                friendId
+            )
+        }
+            .map { response ->
+                response.data!!
+            }
 }
