ec25c8ee940d75362fe3f3975ad6e835481b97a9
이빵주 <yungju9982@naver.com>
2023-01-12 18:59:02 +0900

    #28 [add] getBookToBarcode 네이버 데이터소스 추가

--- app/src/main/java/com/sopt/peekabookaos/data/source/remote/NaverDataSource.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/source/remote/NaverDataSource.kt
@@ -1,8 +1,12 @@
 package com.sopt.peekabookaos.data.source.remote
+
+import com.sopt.peekabookaos.data.entity.response.NaverBookResponse
 import com.sopt.peekabookaos.data.service.NaverService
 import javax.inject.Inject
 
 class NaverDataSource @Inject constructor(
     private val naverService: NaverService
 ) {
+    suspend fun getBookToBarcode(isbn: String): NaverBookResponse =
+        naverService.getBookToBarcode(isbn)
 }
