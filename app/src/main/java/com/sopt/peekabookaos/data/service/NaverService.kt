78a59daa6773c4fcad3e1fd074b2a883a6de63a2
이빵주 <yungju9982@naver.com>
2023-01-12 19:00:45 +0900

    #28 [add] getBookToBarcode 서비스 get 추가

--- app/src/main/java/com/sopt/peekabookaos/data/service/NaverService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/NaverService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NaverService {
+    @GET("search/book_adv.json")
     suspend fun getBookToBarcode(
         @Query("d_isbn") isbn: String
     ): NaverBookResponse
