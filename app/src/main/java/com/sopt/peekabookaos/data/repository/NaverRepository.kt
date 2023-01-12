666e815377c7fe67f66d150acd86d64789579144
이빵주 <yungju9982@naver.com>
2023-01-12 18:59:21 +0900

    #28 [add] getBookToBarcode 네이버 레포지토리 추가

--- app/src/main/java/com/sopt/peekabookaos/data/repository/NaverRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/NaverRepository.kt
@@ -1,4 +1,7 @@
 package com.sopt.peekabookaos.data.repository
 
+import com.sopt.peekabookaos.data.entity.Book
+
 interface NaverRepository {
+    suspend fun getBookToBarcode(isbn: String): Result<Book>
 }
