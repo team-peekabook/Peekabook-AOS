202728b8be8675e810fdbd15783b1f7b65ab3901
이빵주 <yungju9982@naver.com>
2023-01-12 18:56:52 +0900

    #28 [add] 네이버 데이터소스 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/data/source/remote/NaverDataSource.kt
@@ -0,0 +1,8 @@
+package com.sopt.peekabookaos.data.source.remote
+import com.sopt.peekabookaos.data.service.NaverService
+import javax.inject.Inject
+
+class NaverDataSource @Inject constructor(
+    private val naverService: NaverService
+) {
+}
