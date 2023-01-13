e58286c7e625271fd6e0e08cfd5eee667d569efd
Stellarhalo <stellarhalo@naver.com>
2023-01-13 23:22:28 +0900

    #95 [fix] splash, launch icon 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/splash/SplashActivity.kt
@@ -3,13 +3,14 @@ package com.sopt.peekabookaos.presentation.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.peekabookaos.presentation.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
