82ab6e3d86a2d16a82dd3cfef86ba3bff0b458ed
Kim hajeong <happykhj67@gmail.com>
2023-01-13 21:51:51 +0900

    #89 [fix] book data 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/createUpdateBook/CreateUpdateBookActivity.kt
@@ -41,7 +41,7 @@ class CreateUpdateBookActivity :
             else -> {
                 createUpdateBookViewModel.initUiState(
                     bookData = intent.getParcelable(BOOK, Book::class.java)!!,
-                    bookComment = BookComment(),
+                    bookComment = BookComment("", ""),
                     update = false
                 )
             }
