eee602ab5e6d22ada1614b1f8e16ede7ed6bbf1d
이빵주 <yungju9982@naver.com>
2023-02-09 13:00:00 +0900

    #108 [add] BookActivity 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.peekabookaos.presentation.book
+
+import android.os.Bundle
+import com.sopt.peekabookaos.R
+import com.sopt.peekabookaos.databinding.ActivityBookBinding
+import com.sopt.peekabookaos.util.binding.BindingActivity
+
+class BookActivity : BindingActivity<ActivityBookBinding>(R.layout.activity_book) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
