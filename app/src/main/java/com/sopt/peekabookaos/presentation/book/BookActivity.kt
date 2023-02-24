e1249b65ddcd92d1c82f359590187566cdad8f8c
이빵주 <yungju9982@naver.com>
2023-02-24 17:53:39 +0900

    #112 [add] 북 액티비티에 AndroidEntryPoint 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BookActivity : BindingActivity<ActivityBookBinding>(R.layout.activity_book) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
