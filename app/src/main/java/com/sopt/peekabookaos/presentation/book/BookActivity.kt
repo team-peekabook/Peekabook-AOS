e3468f12de56ace3e1c30984dde82ba0e921c1a6
이빵주 <yungju9982@naver.com>
2023-02-26 17:28:33 +0900

    #112 [add] 메인액티비티에서 북액티비티로 이동시 책검색뷰 초기화 함수 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
@@ -1,6 +1,7 @@
 package com.sopt.peekabookaos.presentation.book
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
@@ -10,6 +11,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class BookActivity : BindingActivity<ActivityBookBinding>(R.layout.activity_book) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initSearchFragmentView()
+    }
+
+    private fun initSearchFragmentView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fc_book) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        when (intent.getStringExtra(LOCATION) ?: CREATE) {
+            RECOMMEND -> {
+                navController.navigate(R.id.searchBookFragment)
+            }
+            else -> {
+                navController.navigate(R.id.barcodeScannerFragment)
+            }
+        }
+    }
+
     companion object {
         const val UPDATE = "update"
         const val BOOK_COMMENT = "book_comment"
