ad4154021a6b151be89351d07c268df7c84d8bb3
이빵주 <yungju9982@naver.com>
2023-02-26 19:21:28 +0900

    #112 [chore] ktlint

--- app/src/main/java/com/sopt/peekabookaos/presentation/createBook/CreateBookFragment.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/createBook/CreateBookFragment.kt
@@ -6,7 +6,8 @@ import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.LayoutCreateUpdateBookBinding
 import com.sopt.peekabookaos.util.binding.BindingFragment
 
-class CreateBookFragment : BindingFragment<LayoutCreateUpdateBookBinding>(R.layout.layout_create_update_book) {
+class CreateBookFragment :
+    BindingFragment<LayoutCreateUpdateBookBinding>(R.layout.layout_create_update_book) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
