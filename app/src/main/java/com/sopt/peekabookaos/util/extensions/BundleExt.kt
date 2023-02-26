de8f8c0a2bdfa2bcabc9b917b33b9d40ee609c8e
이빵주 <yungju9982@naver.com>
2023-02-26 17:18:13 +0900

    #112 [add] Bundle에서 사용하는 getParcelable 확장함수 추가

---
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/BundleExt.kt
@@ -0,0 +1,12 @@
+package com.sopt.peekabookaos.util.extensions
+
+import android.os.Build
+import android.os.Bundle
+
+fun <T> Bundle.getParcelableCompat(name: String, clazz: Class<T>): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelable(name, clazz)
+    } else {
+        getParcelable(name)
+    }
+}
