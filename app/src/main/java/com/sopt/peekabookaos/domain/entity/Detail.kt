4f7158a57ac25aa0b143c5bf3889a0d63226b5c7
Kim hajeong <happykhj67@gmail.com>
2023-02-27 20:14:46 +0900

    #107 [delete] Serializable 어노테이션 삭제

--- app/src/main/java/com/sopt/peekabookaos/domain/entity/Detail.kt
+++ app/src/main/java/com/sopt/peekabookaos/domain/entity/Detail.kt
@@ -3,9 +3,7 @@ package com.sopt.peekabookaos.domain.entity
 import android.os.Parcelable
 import com.sopt.peekabookaos.data.entity.Book
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Serializable
 
-@Serializable
 @Parcelize
 data class Detail(
     val description: String?,
