0b289627e857d16d49edffce28e35b7fcd21c310
Kim hajeong <happykhj67@gmail.com>
2023-02-28 14:57:15 +0900

    #113 [add] 도메인 변경을 위한 함수 추가

--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendationResponse.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendationResponse.kt
@@ -1,8 +1,13 @@
 package com.sopt.peekabookaos.data.entity.response
 
+import com.sopt.peekabookaos.domain.entity.Recommendation
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RecommendationResponse(
     val recommendId: Int
-)
+) {
+    fun toRecommendation(): Recommendation = Recommendation(
+        recommendId = this.recommendId
+    )
+}
