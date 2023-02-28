3a326f08f34505a918b0c71e23df75bc6d31200a
Kim hajeong <happykhj67@gmail.com>
2023-02-28 15:00:47 +0900

    #113 [add] 유즈케이스 추가

---
+++ app/src/main/java/com/sopt/peekabookaos/domain/usecase/PostRecommendationUseCase.kt
@@ -0,0 +1,15 @@
+package com.sopt.peekabookaos.domain.usecase
+
+import com.sopt.peekabookaos.data.entity.request.RecommendationRequest
+import com.sopt.peekabookaos.domain.repository.RecommendRepository
+import javax.inject.Inject
+
+class PostRecommendationUseCase @Inject constructor(
+    private val recommendRepository: RecommendRepository
+) {
+    suspend operator fun invoke(
+        recommendationRequest: RecommendationRequest,
+        friendId: Int
+    ) =
+        recommendRepository.postRecommendation(recommendationRequest, friendId)
+}
