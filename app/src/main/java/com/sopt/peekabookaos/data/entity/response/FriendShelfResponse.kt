a42bb6dad0748651c029914627d31bbb78d249f1
river99 <qkrrkd42@naver.com>
2023-03-01 18:44:14 +0900

    #120 [refactor] friendBookshelf/pick domain 변경

--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/FriendShelfResponse.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/response/FriendShelfResponse.kt
@@ -4,6 +4,7 @@ import com.sopt.peekabookaos.data.entity.Books
 import com.sopt.peekabookaos.data.entity.FriendList
 import com.sopt.peekabookaos.data.entity.Picks
 import com.sopt.peekabookaos.data.entity.SelfIntro
+import com.sopt.peekabookaos.domain.entity.FriendShelf
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,4 +15,13 @@ data class FriendShelfResponse(
     val picks: List<Picks>,
     val bookTotalNum: Int,
     val books: List<Books>
-)
+){
+    fun toFriendShelf(): FriendShelf = FriendShelf(
+        myIntro = this.myIntro,
+        friendList = this.friendList,
+        friendIntro = this.friendIntro,
+        picks = this.picks,
+        bookTotalNum = this.bookTotalNum,
+        books = this.books
+    )
+}
