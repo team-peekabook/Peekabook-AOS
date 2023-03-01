c8b4e05585d23f96d34b5b4666a37b00f30fae95
Kim hajeong <happykhj67@gmail.com>
2023-03-01 16:41:09 +0900

    #119 [fix] 데이터 클래스 RecommendResponse에서 Recommend로 변경

--- app/src/main/java/com/sopt/peekabookaos/domain/repository/RecommendRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/domain/repository/RecommendRepository.kt
@@ -1,11 +1,11 @@
 package com.sopt.peekabookaos.domain.repository
 
 import com.sopt.peekabookaos.data.entity.request.RecommendationRequest
-import com.sopt.peekabookaos.data.entity.response.RecommendResponse
+import com.sopt.peekabookaos.domain.entity.Recommend
 import com.sopt.peekabookaos.domain.entity.Recommendation
 
 interface RecommendRepository {
-    suspend fun getRecommend(): Result<RecommendResponse>
+    suspend fun getRecommend(): Result<Recommend>
 
     suspend fun postRecommendation(
         recommendationRequest: RecommendationRequest,
