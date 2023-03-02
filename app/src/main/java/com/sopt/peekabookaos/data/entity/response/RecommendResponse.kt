69e44a650482591905267e637315bede3f0ac4eb
Kim hajeong <happykhj67@gmail.com>
2023-03-02 10:04:21 +0900

    #119 [chore] Recommend 데이터클래스 import

--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendResponse.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendResponse.kt
@@ -1,16 +1,16 @@
 package com.sopt.peekabookaos.data.entity.response
 
-import com.sopt.peekabookaos.data.entity.Recommend
+import com.sopt.peekabookaos.data.entity.RecommendEntity
+import com.sopt.peekabookaos.domain.entity.Recommend
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RecommendResponse(
-    val recommendedBook: List<Recommend>,
-    val recommendingBook: List<Recommend>
+    val recommendedBook: List<RecommendEntity>,
+    val recommendingBook: List<RecommendEntity>
 ) {
-    fun toRecommend(): com.sopt.peekabookaos.domain.entity.Recommend =
-        com.sopt.peekabookaos.domain.entity.Recommend(
-            recommendedBook = this.recommendedBook,
-            recommendingBook = this.recommendingBook
-        )
+    fun toRecommend(): Recommend = Recommend(
+        recommendedBook = this.recommendedBook,
+        recommendingBook = this.recommendingBook
+    )
 }
