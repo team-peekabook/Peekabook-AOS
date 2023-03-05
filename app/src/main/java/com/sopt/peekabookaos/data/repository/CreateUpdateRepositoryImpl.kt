97051a12a99f16ebe14c338223e83ef7525b77e3
이빵주 <yungju9982@naver.com>
2023-03-06 02:35:09 +0900

    #124 [fix] CreateUpdateRepositoryImpl 리퀘스트 수정 및 response map

--- app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/CreateUpdateRepositoryImpl.kt
@@ -1,21 +1,39 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.BookComment
-import com.sopt.peekabookaos.data.entity.NoResponse
-import com.sopt.peekabookaos.data.entity.request.CreateBookRequest
-import com.sopt.peekabookaos.data.entity.response.CreateBookResponse
 import com.sopt.peekabookaos.data.source.remote.CreateUpdateDataSource
+import com.sopt.peekabookaos.domain.entity.Book
+import com.sopt.peekabookaos.domain.repository.CreateUpdateRepository
 import javax.inject.Inject
 
 class CreateUpdateRepositoryImpl @Inject constructor(
     private val createUpdateDataSource: CreateUpdateDataSource
 ) : CreateUpdateRepository {
-    override suspend fun postCreateBook(createBookRequest: CreateBookRequest): Result<CreateBookResponse> =
-        kotlin.runCatching { createUpdateDataSource.postCreateBook(createBookRequest) }
+    override suspend fun postCreateBook(
+        bookImage: String,
+        bookTitle: String,
+        author: String,
+        description: String?,
+        memo: String?
+    ): Result<Book> =
+        kotlin.runCatching {
+            createUpdateDataSource.postCreateBook(
+                bookImage,
+                bookTitle,
+                author,
+                description,
+                memo
+            )
+        }
             .map { response ->
-                response.data!!
+                requireNotNull(response.data).toBook()
             }
 
-    override suspend fun patchBook(bookId: Int, bookComment: BookComment): Result<NoResponse> =
-        kotlin.runCatching { createUpdateDataSource.patchBook(bookId, bookComment) }
+    override suspend fun patchBook(
+        bookId: Int,
+        description: String?,
+        memo: String?
+    ): Result<Boolean> =
+        kotlin.runCatching {
+            createUpdateDataSource.patchBook(bookId, description, memo)
+        }.map { response -> response.success }
 }
