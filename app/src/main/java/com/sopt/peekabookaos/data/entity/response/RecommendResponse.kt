a0446a020903596667316b5ee6993e04226bd752
Kim hajeong <happykhj67@gmail.com>
2023-03-10 02:07:18 +0900

    #119 [fix] 함수 변경

--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendResponse.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendResponse.kt
@@ -1,6 +1,6 @@
 package com.sopt.peekabookaos.data.entity.response
 
-import com.sopt.peekabookaos.data.entity.RecommendEntity
+import com.sopt.peekabookaos.domain.entity.RecommendBookList
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -8,8 +8,12 @@ data class RecommendResponse(
     val recommendedBook: List<RecommendEntity>,
     val recommendingBook: List<RecommendEntity>
 ) {
-    fun toRecommend(): Recommend = Recommend(
-        recommendedBook = recommendedBook,
-        recommendingBook = recommendingBook
+    fun toRecommendBookList(): RecommendBookList = RecommendBookList(
+        recommendedBook = this.recommendedBook.map { recommendEntity ->
+            recommendEntity.toRecommend()
+        },
+        recommendingBook = this.recommendingBook.map { recommendEntity ->
+            recommendEntity.toRecommend()
+        }
     )
 }
