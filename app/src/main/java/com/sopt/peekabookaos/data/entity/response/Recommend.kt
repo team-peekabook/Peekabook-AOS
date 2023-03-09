c61e911c045e8b5be65a209844a4451da3c66084
Kim hajeong <happykhj67@gmail.com>
2023-03-10 01:56:09 +0900

    #119 [add] toRecommendBookList() 함수 추가

--- app/src/main/java/com/sopt/peekabookaos/data/entity/response/Recommend.kt
+++
@@ -1,8 +0,0 @@
-package com.sopt.peekabookaos.data.entity.response
-
-import com.sopt.peekabookaos.data.entity.RecommendEntity
-
-data class Recommend(
-    val recommendedBook: List<RecommendEntity>,
-    val recommendingBook: List<RecommendEntity>
-)
---
+++ app/src/main/java/com/sopt/peekabookaos/data/entity/response/RecommendEntity.kt
@@ -0,0 +1,31 @@
+package com.sopt.peekabookaos.data.entity.response
+
+import com.sopt.peekabookaos.domain.entity.Recommend
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RecommendEntity(
+    val recommendId: Int,
+    val recommendDesc: String?,
+    val createdAt: String,
+    val friendId: Int,
+    val friendNickname: String,
+    val friendImage: String,
+    val bookId: Int,
+    val bookTitle: String,
+    val author: String,
+    val bookImage: String
+) {
+    fun toRecommend(): Recommend = Recommend(
+        recommendId = this.recommendId,
+        recommendDesc = this.recommendDesc,
+        createdAt = this.createdAt,
+        friendId = this.recommendId,
+        friendNickname = this.friendNickname,
+        friendImage = this.friendImage,
+        bookId = this.bookId,
+        bookTitle = this.bookTitle,
+        author = this.author,
+        bookImage = this.bookImage
+    )
+}
