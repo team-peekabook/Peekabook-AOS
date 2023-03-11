1d0eecca1bdde6bd885c001c5fa68719cd7455eb
이빵주 <yungju9982@naver.com>
2023-03-11 18:45:44 +0900

    #116 [refactor] 책 등록 액티비티에서 프래그먼트로 변경

--- app/src/main/java/com/sopt/peekabookaos/presentation/createBook/CreateBookFragment.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/createBook/CreateBookFragment.kt
@@ -1,14 +1,85 @@
 package com.sopt.peekabookaos.presentation.createBook
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.sopt.peekabookaos.R
-import com.sopt.peekabookaos.databinding.LayoutCreateUpdateBookBinding
+import com.sopt.peekabookaos.databinding.FragmentCreateBookBinding
+import com.sopt.peekabookaos.domain.entity.Book
+import com.sopt.peekabookaos.presentation.book.BookActivity.Companion.BOOK
+import com.sopt.peekabookaos.presentation.book.BookActivity.Companion.BOOK_ID
+import com.sopt.peekabookaos.presentation.detail.DetailActivity
 import com.sopt.peekabookaos.util.binding.BindingFragment
+import com.sopt.peekabookaos.util.extensions.KeyBoardUtil
+import com.sopt.peekabookaos.util.extensions.UiEvent
+import com.sopt.peekabookaos.util.extensions.getParcelableCompat
+import com.sopt.peekabookaos.util.extensions.repeatOnStarted
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CreateBookFragment :
-    BindingFragment<LayoutCreateUpdateBookBinding>(R.layout.layout_create_update_book) {
+    BindingFragment<FragmentCreateBookBinding>(R.layout.fragment_create_book) {
+    private val createBookViewModel: CreateBookViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.vm = createBookViewModel
+        initBookInfo()
+        initEditTextClearFocus()
+        initCloseBtnOnClickListener()
+        isPostCollect()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initEditTextClearFocus() {
+        binding.clCreateBook.setOnTouchListener { _, _ ->
+            KeyBoardUtil.hide(activity = requireActivity())
+            return@setOnTouchListener false
+        }
+
+        binding.btnCreateBookSave.setOnTouchListener { _, _ ->
+            KeyBoardUtil.hide(activity = requireActivity())
+            return@setOnTouchListener false
+        }
+    }
+
+    private fun initBookInfo() {
+        createBookViewModel.initBookInfo(
+            bookInfo = arguments?.getParcelableCompat(BOOK, Book::class.java) ?: Book()
+        )
+    }
+
+    private fun initCloseBtnOnClickListener() {
+        binding.btnCreateBookClose.setOnClickListener {
+            activity?.finish()
+        }
+    }
+
+    private fun isPostCollect() {
+        repeatOnStarted {
+            createBookViewModel.uiEvent.collect { uiEvent ->
+                when (uiEvent) {
+                    UiEvent.SUCCESS -> {
+                        startActivity(
+                            Intent(requireActivity(), DetailActivity::class.java).apply {
+                                putExtra(
+                                    BOOK_ID,
+                                    createBookViewModel.uiState.value.bookInfo.id
+                                )
+                            }
+                        )
+                        activity?.finish()
+                    }
+                    UiEvent.ERROR -> {
+                        return@collect
+                    }
+                    UiEvent.IDLE -> {
+                        return@collect
+                    }
+                }
+            }
+        }
     }
 }
