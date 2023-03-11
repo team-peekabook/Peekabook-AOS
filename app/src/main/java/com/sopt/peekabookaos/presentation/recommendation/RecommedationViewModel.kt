1b40162c25fb8379a0c2a698900a6fd62923363b
Kim hajeong <happykhj67@gmail.com>
2023-03-11 22:25:00 +0900

    #126 [delete] data.entity.SelfIntro 삭제

--- app/src/main/java/com/sopt/peekabookaos/presentation/recommendation/RecommedationViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/recommendation/RecommedationViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sopt.peekabookaos.data.entity.SelfIntro
 import com.sopt.peekabookaos.domain.entity.Book
 import com.sopt.peekabookaos.domain.entity.SelfIntro
 import com.sopt.peekabookaos.domain.usecase.PostRecommendationUseCase
