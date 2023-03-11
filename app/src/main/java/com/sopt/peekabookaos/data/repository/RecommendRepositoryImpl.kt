5e82a47a491e2805a6945c56669dc1240598f38f
Kim hajeong <happykhj67@gmail.com>
2023-03-11 17:21:28 +0900

    #126 [fix] 반환값 수정

--- app/src/main/java/com/sopt/peekabookaos/data/repository/RecommendRepositoryImpl.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/RecommendRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.sopt.peekabookaos.data.repository
 
-import com.sopt.peekabookaos.data.entity.response.RecommendResponse
 import com.sopt.peekabookaos.data.source.remote.RecommendDataSource
+import com.sopt.peekabookaos.domain.entity.RecommendBookList
 import com.sopt.peekabookaos.domain.entity.Recommendation
 import com.sopt.peekabookaos.domain.repository.RecommendRepository
 import javax.inject.Inject
@@ -9,9 +9,9 @@ import javax.inject.Inject
 class RecommendRepositoryImpl @Inject constructor(
     private val recommendDataSource: RecommendDataSource
 ) : RecommendRepository {
-    override suspend fun getRecommend(): Result<RecommendResponse> =
+    override suspend fun getRecommend(): Result<RecommendBookList> =
         kotlin.runCatching { recommendDataSource.getRecommend() }.map { response ->
-            response.data!!
+            requireNotNull(response.data).toRecommendBookList()
         }
 
     override suspend fun postRecommendation(
