d352b09b96ffd67fe8040a8bd8e2fd39225e4c3a
Kim hajeong <happykhj67@gmail.com>
2023-03-11 17:22:24 +0900

    #126 [fix] import 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/recommendation/RecommedationViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/recommendation/RecommedationViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.data.entity.SelfIntro
+import com.sopt.peekabookaos.domain.entity.Book
 import com.sopt.peekabookaos.domain.usecase.PostRecommendationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
