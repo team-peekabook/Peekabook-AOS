dccc49d7e8409807ebbaa2f35c784a181bd9762e
이빵주 <yungju9982@naver.com>
2023-03-16 04:43:04 +0900

    #134 [add] Event 익스텐션 추가

---
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/EventExt.kt
@@ -0,0 +1,15 @@
+package com.sopt.peekabookaos.util.extensions
+
+class Event<T>(private val content: T) {
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
