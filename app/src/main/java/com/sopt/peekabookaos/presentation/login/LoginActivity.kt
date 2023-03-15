8671d09a9e8ff3fcba372c173210279fac3a3ff2
이빵주 <yungju9982@naver.com>
2023-03-16 04:46:58 +0900

    #134 [add] 로그인 액티비티 생성

---
+++ app/src/main/java/com/sopt/peekabookaos/presentation/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+@AndroidEntryPoint
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
