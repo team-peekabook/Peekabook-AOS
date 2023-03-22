5b1bf2355941cb2afdb3fc3382e1fef5f3f9e7c4
이빵주 <yungju9982@naver.com>
2023-03-22 15:42:01 +0900

    #134 [fix] KakaoLoginCallback 호출하여 카카오 로그인 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/login/LoginViewModel.kt
@@ -2,8 +2,7 @@ package com.sopt.peekabookaos.presentation.login
 
 import androidx.lifecycle.ViewModel
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.AuthErrorCause
-import com.sopt.peekabookaos.util.Event
+import com.sopt.peekabookaos.util.KakaoLoginCallback
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -19,50 +18,17 @@ class LoginViewModel : ViewModel() {
     val isSignedUp = _isSignedUp.asSharedFlow()
 
     val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if (error != null) {
-            when {
-                error.toString() == AuthErrorCause.AccessDenied.toString() -> {
-                    Timber.e(error, "접근이 거부 됨(동의 취소)")
-                }
-                error.toString() == AuthErrorCause.InvalidClient.toString() -> {
-                    Timber.e(error, "유효하지 않은 앱")
-                }
-                error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
-                    Timber.e(error, "인증 수단이 유효하지 않아 인증할 수 없는 상태")
-                }
-                error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
-                    Timber.e(error, "요청 파라미터 오류")
-                }
-                error.toString() == AuthErrorCause.InvalidScope.toString() -> {
-                    Timber.e(error, "유효하지 않은 scope ID")
-                }
-                error.toString() == AuthErrorCause.Misconfigured.toString() -> {
-                    Timber.e(error, "설정이 올바르지 않음(android key hash)")
-                }
-                error.toString() == AuthErrorCause.ServerError.toString() -> {
-                    Timber.e(error, "서버 내부 에러")
-                }
-                error.toString() == AuthErrorCause.Unauthorized.toString() -> {
-                    Timber.e(error, "앱이 요청 권한이 없음")
-                }
-                else -> {
-                    Timber.e(error, "기타 에러")
-                }
-            }
-        } else if (token != null) {
-            Timber.e("카카오 로그인 성공 ${token.accessToken}")
-            _uiState.value = _uiState.value.copy(kakaoToken = token.accessToken)
-            checkTokenAvailability(_uiState.value.kakaoToken, _uiState.value.fcmToken)
-        }
+        KakaoLoginCallback(_uiState) { checkTokenAvailability() }.handleResult(token, error)
     }
 
-    private fun checkTokenAvailability(kakaoToken: String, fcmToken: String) {
-        val isAvailable = kakaoToken.isNotBlank() && fcmToken.isNotBlank()
-        _uiState.value = _uiState.value.copy(isTokenAvailability = Event(isAvailable))
+    private fun checkTokenAvailability() {
+        val isAvailable =
+            _uiState.value.kakaoToken.isNotBlank() && _uiState.value.fcmToken.isNotBlank()
+        _uiState.value = _uiState.value.copy(isTokenAvailability = isAvailable)
     }
 
     private fun getFcmToken() {
-        /** fcm 토큰 받아오는 로직 */
+        // TODO by 이빵주 fcm 토큰 받아오는 로직
     }
 
     data class LoginUiState(
