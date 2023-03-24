d743cecc65f99de7672e5a7234fadd6e4f585e4b
이빵주 <yungju9982@naver.com>
2023-03-24 13:00:00 +0900

    #150 [fix] 소셜로그인프래그먼트 백버튼 수정

--- app/src/main/java/com/sopt/peekabookaos/presentation/socialLogin/SocialLoginFragment.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/socialLogin/SocialLoginFragment.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.fragment.app.viewModels
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.data.service.KakaoLoginService
 import com.sopt.peekabookaos.databinding.FragmentSocialLoginBinding
+import com.sopt.peekabookaos.util.ToastMessageUtil
 import com.sopt.peekabookaos.util.binding.BindingFragment
 import com.sopt.peekabookaos.util.extensions.repeatOnStarted
 import com.sopt.peekabookaos.util.extensions.setSingleOnClickListener
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class SocialLoginFragment :
@@ -23,29 +28,37 @@ class SocialLoginFragment :
     private var onBackPressedTime = 0L
     private val socialLoginViewModel: SocialLoginViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBackPressedCallback()
         initKakaoLoginBtnClickListener()
         initTermsOfServiceClickListener()
         initPrivacyPolicyClickListener()
         collectIsTokenAvailability()
     }
 
-//    private fun initBackPressedCallback() {
-//        onBackPressedDispatcher.addCallback {
-//            if (System.currentTimeMillis() - onBackPressedTime >= WAITING_DEADLINE) {
-//                onBackPressedTime = System.currentTimeMillis()
-//                ToastMessageUtil.showToast(
-//                    this@LoginFragment,
-//                    getString(R.string.finish_app_toast_msg)
-//                )
-//            } else {
-//                finishAffinity()
-//                System.runFinalization()
-//                exitProcess(0)
-//            }
-//        }
-//    }
+    private fun initBackPressedCallback() {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    val curTime = System.currentTimeMillis()
+                    val gap = curTime - onBackPressedTime
+                    if (gap > WAITING_DEADLINE) {
+                        onBackPressedTime = curTime
+                        ToastMessageUtil.showToast(
+                            requireContext(),
+                            getString(R.string.finish_app_toast_msg)
+                        )
+                        return
+                    }
+                    finishAffinity(requireActivity())
+                    System.runFinalization()
+                    exitProcess(0)
+                }
+            }
+        )
+    }
 
     private fun initKakaoLoginBtnClickListener() {
         binding.btnSocialLoginKakao.setSingleOnClickListener {
