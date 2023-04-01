fe8b20eaf932f2bcc59040701ced95c2f5d58e08
river99 <qkrrkd42@naver.com>
2023-04-01 20:29:44 +0900

    #131 [ui] bottoomsheet > 카메라 연결 및 사진 불러오기

--- app/src/main/java/com/sopt/peekabookaos/presentation/userInput/UserInputBottomSheetFragment.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/userInput/UserInputBottomSheetFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.DialogUserInputBottomSheetBinding
 
 class UserInputBottomSheetFragment(val itemClick: (Int) -> Unit) : BottomSheetDialogFragment() {
@@ -14,8 +13,10 @@ class UserInputBottomSheetFragment(val itemClick: (Int) -> Unit) : BottomSheetDi
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View =
-        inflater.inflate(R.layout.dialog_user_input_bottom_sheet, container, false)
+    ): View? {
+        binding = DialogUserInputBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
