e011c013d45e39e333c894a3bb932f5f6c1cf633
Kim hajeong <happykhj67@gmail.com>
2023-04-03 03:50:03 +0900

    #141 [delete] Timber 삭제

--- app/src/main/java/com/sopt/peekabookaos/presentation/report/ReportViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/report/ReportViewModel.kt
@@ -1,16 +1,14 @@
 package com.sopt.peekabookaos.presentation.report
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import timber.log.Timber
 
 class ReportViewModel : ViewModel() {
     private val _selectedReasonId = MutableLiveData(1)
-    val selectedReasonId: LiveData<Int> = _selectedReasonId
+
     val reason = MutableLiveData("")
+
     fun setSelectedReasonId(id: Int) {
         _selectedReasonId.value = id
-        Timber.d("${selectedReasonId.value}")
     }
 }
