7154f1cd83bd19613c7f7cbb389aa771cbcd57f0
Kim hajeong <happykhj67@gmail.com>
2023-04-05 18:09:56 +0900

    #165 [add] Block 레포지토리Impl 추가

---
+++ app/src/main/java/com/sopt/peekabookaos/data/repository/BlockRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.sopt.peekabookaos.data.repository
+
+import com.sopt.peekabookaos.data.source.remote.BlockDataSource
+import com.sopt.peekabookaos.domain.entity.FriendList
+import com.sopt.peekabookaos.domain.repository.BlockRepository
+import javax.inject.Inject
+
+class BlockRepositoryImpl @Inject constructor(
+    private val blockDataSource: BlockDataSource
+) : BlockRepository {
+    override suspend fun getBlock(): Result<List<FriendList>> =
+        kotlin.runCatching { blockDataSource.getBlock() }.map { response ->
+            requireNotNull(response.data).map { blockEntity ->
+                blockEntity.toBlock()
+            }
+        }
+}
