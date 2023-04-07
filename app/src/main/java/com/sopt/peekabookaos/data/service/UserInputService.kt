2e57463fdad5a9855bbf2b9088870301109e49da
river99 <qkrrkd42@naver.com>
2023-04-08 04:05:37 +0900

    #163 [chore] UserInputService wild card 삭제

--- app/src/main/java/com/sopt/peekabookaos/data/service/UserInputService.kt
+++ app/src/main/java/com/sopt/peekabookaos/data/service/UserInputService.kt
@@ -6,7 +6,12 @@ import com.sopt.peekabookaos.data.entity.request.DuplicateRequest
 import com.sopt.peekabookaos.data.entity.response.DuplicateResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.Multipart
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.PartMap
 
 interface UserInputService {
     @POST("user/duplicate")
