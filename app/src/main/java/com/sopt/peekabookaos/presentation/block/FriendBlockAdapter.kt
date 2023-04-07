0f369fd1c56a7267b90cf0b082245d83c88e3ecb
Kim hajeong <happykhj67@gmail.com>
2023-04-08 02:53:17 +0900

    #165 [fix] 람다식으로 함수 전달, tvBlockCancel 버튼 바인딩하기

--- app/src/main/java/com/sopt/peekabookaos/presentation/block/FriendBlockAdapter.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/block/FriendBlockAdapter.kt
@@ -1,7 +1,6 @@
 package com.sopt.peekabookaos.presentation.block
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,50 +8,26 @@ import com.sopt.peekabookaos.databinding.ItemBlockBinding
 import com.sopt.peekabookaos.domain.entity.FriendList
 import com.sopt.peekabookaos.util.ItemDiffCallback
 
-class FriendBlockAdapter :
-    ListAdapter<FriendList, FriendBlockAdapter.FriendBlockViewHolder>(
-        blockDiffUtil
-    ) {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): FriendBlockViewHolder {
-        val binding =
-            ItemBlockBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        return FriendBlockViewHolder(
-            binding
-        )
-    }
-
-    override fun onBindViewHolder(
-        holder: FriendBlockViewHolder,
-        position: Int
-    ) {
-        holder.onBind(getItem(position))
-        holder.itemView.setOnClickListener {
-            itemClickListener.onClick(it, position)
-        }
-    }
-
-    interface OnItemClickListener {
-        fun onClick(v: View, position: Int)
-    }
-
-    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
-        this.itemClickListener = onItemClickListener
-    }
-
-    private lateinit var itemClickListener: OnItemClickListener
-
-    class FriendBlockViewHolder(private val binding: ItemBlockBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun onBind(blockFriend: FriendList) {
+class FriendBlockAdapter(private val showBlockCancelDialog: () -> Unit) :
+    ListAdapter<FriendList, FriendBlockAdapter.FriendBlockViewHolder>(blockDiffUtil) {
+    class FriendBlockViewHolder(
+        private val binding: ItemBlockBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(blockFriend: FriendList, showBlockCancelDialog: () -> Unit) {
             binding.data = blockFriend
+            binding.tvBlockCancel.setOnClickListener {
+                showBlockCancelDialog()
+            }
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendBlockViewHolder {
+        val binding = ItemBlockBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FriendBlockViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FriendBlockViewHolder, position: Int) {
+        holder.onBind(getItem(position), showBlockCancelDialog)
     }
 
     companion object {
