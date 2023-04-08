aa087246993dc57eb3a134aedf278a0e743ddae2
Kim hajeong <happykhj67@gmail.com>
2023-04-09 07:16:31 +0900

    #175 [add] postReport() 함수 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/report/ReportViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/report/ReportViewModel.kt
@@ -1,14 +1,46 @@
 package com.sopt.peekabookaos.presentation.report
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.peekabookaos.domain.entity.SelfIntro
+import com.sopt.peekabookaos.domain.usecase.PostReportUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class ReportViewModel : ViewModel() {
+@HiltViewModel
+class ReportViewModel @Inject constructor(
+    private val postReportUseCase: PostReportUseCase
+) : ViewModel() {
     private val selectedReasonId = MutableLiveData(1)
+
+    private val _friendData = MutableLiveData<SelfIntro>()
+    val friendData: LiveData<SelfIntro> = _friendData
 
     val reason = MutableLiveData("")
 
     fun setSelectedReasonId(id: Int) {
         selectedReasonId.value = id
+    }
+
+    private val _isReport = MutableLiveData<Boolean>()
+    val isReport: LiveData<Boolean> = _isReport
+
+    fun postReport() {
+        viewModelScope.launch {
+            postReportUseCase(
+                reasonIndex = requireNotNull(selectedReasonId.value),
+                etc = reason.value,
+                requireNotNull(_friendData.value).id
+            ).onSuccess {
+                _isReport.value = true
+            }.onFailure { throwable ->
+                _isReport.value = false
+                Timber.e("$throwable")
+            }
+        }
     }
 }
