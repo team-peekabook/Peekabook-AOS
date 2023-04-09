5ef0868232d405df16aa915a8a7406bee8337626
이빵주 <yungju9982@naver.com>
2023-04-09 14:45:21 +0900

    #177 [ui/feat] 로그아웃 구현

--- app/src/main/java/com/sopt/peekabookaos/presentation/myPage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/myPage/MyPageViewModel.kt
@@ -3,9 +3,23 @@ package com.sopt.peekabookaos.presentation.myPage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.domain.entity.SelfIntro
+import com.sopt.peekabookaos.domain.usecase.ClearLocalPrefUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MyPageViewModel : ViewModel() {
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+    private val clearLocalPrefUseCase: ClearLocalPrefUseCase
+) : ViewModel() {
     private val _userData = MutableLiveData<SelfIntro>()
     val userData: LiveData<SelfIntro> = _userData
+
+    fun clearLocalPref() {
+        viewModelScope.launch {
+            clearLocalPrefUseCase()
+        }
+    }
 }
