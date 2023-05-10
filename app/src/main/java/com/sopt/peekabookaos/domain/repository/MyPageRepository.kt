71c463e2ed203f9bd5983fa6128c1c63d82f5279
Kim hajeong <happykhj67@gmail.com>
2023-05-10 18:39:57 +0900

    #151 [fix] profileModify레포지토리안의 함수 myPage레포지토리에 넣기

--- app/src/main/java/com/sopt/peekabookaos/domain/repository/MyPageRepository.kt
+++ app/src/main/java/com/sopt/peekabookaos/domain/repository/MyPageRepository.kt
@@ -1,7 +1,14 @@
 package com.sopt.peekabookaos.domain.repository
 
 import com.sopt.peekabookaos.domain.entity.User
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface MyPageRepository {
     suspend fun getMyPage(): Result<User>
+
+    suspend fun patchProfileModify(
+        file: MultipartBody.Part?,
+        requestBodyMap: HashMap<String, RequestBody>
+    ): Result<Boolean>
 }
