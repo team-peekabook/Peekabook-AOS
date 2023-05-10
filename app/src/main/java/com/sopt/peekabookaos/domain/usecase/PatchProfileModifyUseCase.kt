a96e661d95a4eb977934ce2bd45391cfa9e5a804
Kim hajeong <happykhj67@gmail.com>
2023-05-10 18:42:06 +0900

    #151 [fix] myPageRepository 주입시키기

--- app/src/main/java/com/sopt/peekabookaos/domain/usecase/PatchProfileModifyUseCase.kt
+++ app/src/main/java/com/sopt/peekabookaos/domain/usecase/PatchProfileModifyUseCase.kt
@@ -1,15 +1,15 @@
 package com.sopt.peekabookaos.domain.usecase
 
-import com.sopt.peekabookaos.domain.repository.ProfileModifyRepository
+import com.sopt.peekabookaos.domain.repository.MyPageRepository
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import javax.inject.Inject
 
 class PatchProfileModifyUseCase @Inject constructor(
-    private val profileModifyRepository: ProfileModifyRepository
+    private val myPageRepository: MyPageRepository
 ) {
     suspend operator fun invoke(
-        file: MultipartBody.Part,
+        file: MultipartBody.Part?,
         requestBodyMap: HashMap<String, RequestBody>
-    ) = profileModifyRepository.patchProfileModify(file, requestBodyMap)
+    ) = myPageRepository.patchProfileModify(file, requestBodyMap)
 }
