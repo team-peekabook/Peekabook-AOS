7bc11eb51b876740794330f2f85fcc858cf6773e
Kim hajeong <happykhj67@gmail.com>
2023-08-16 20:50:28 +0900

    #245 [add] nickname을 observe하기 위해 추가

--- app/src/main/java/com/sopt/peekabookaos/presentation/bookshelf/BookshelfFragment.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/bookshelf/BookshelfFragment.kt
@@ -110,6 +110,10 @@ class BookshelfFragment : BindingFragment<FragmentBookshelfBinding>(R.layout.fra
                 updateToMyShelf()
             }
         }
+
+        viewModel.userData.observe(viewLifecycleOwner) { user ->
+            binding.tvBookshelfUserProfileName.text = user.nickname
+        }
     }
 
     private fun initDataObserver() {
