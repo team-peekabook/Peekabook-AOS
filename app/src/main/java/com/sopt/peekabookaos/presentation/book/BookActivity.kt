11b3a95b5cbde7debaedaff95637ad0b93f65bd2
이빵주 <yungju9982@naver.com>
2023-10-24 13:00:00 +0900

    #276 [feat] BookActivity에 화면전환애니메이션 확장함수 적용

--- app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/book/BookActivity.kt
@@ -1,10 +1,12 @@
 package com.sopt.peekabookaos.presentation.book
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityBookBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
+import com.sopt.peekabookaos.util.extensions.activityTransition
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,18 +24,31 @@ class BookActivity : BindingActivity<ActivityBookBinding>(R.layout.activity_book
         when (intent.getStringExtra(LOCATION) ?: CREATE) {
             RECOMMEND -> {
                 navController.navigate(R.id.action_barcodeScannerFragment_to_searchBookFragment)
-                overridePendingTransition(R.animator.anim_from_right, R.animator.anim_to_left)
+                activityTransition(
+                    Activity.OVERRIDE_TRANSITION_OPEN,
+                    R.animator.anim_from_right,
+                    R.animator.anim_to_left
+                )
             }
+
             else -> {
                 navController.navigate(R.id.barcodeScannerFragment)
-                overridePendingTransition(R.animator.anim_from_bottom, R.animator.anim_to_top)
+                activityTransition(
+                    Activity.OVERRIDE_TRANSITION_OPEN,
+                    R.animator.anim_from_bottom,
+                    R.animator.anim_to_top
+                )
             }
         }
     }
 
     override fun finish() {
         super.finish()
-        overridePendingTransition(R.animator.anim_from_left, R.animator.anim_to_right)
+        activityTransition(
+            Activity.OVERRIDE_TRANSITION_CLOSE,
+            R.animator.anim_from_left,
+            R.animator.anim_to_right
+        )
     }
 
     companion object {
