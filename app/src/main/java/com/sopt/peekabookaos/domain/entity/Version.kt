24baa9982da039feeb9bd2910694928e58ac4cd1
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-11-05 23:38:44 +0900

    #273[fix] Parcelable 추가

--- app/src/main/java/com/sopt/peekabookaos/domain/entity/Version.kt
+++ app/src/main/java/com/sopt/peekabookaos/domain/entity/Version.kt
@@ -1,7 +1,11 @@
 package com.sopt.peekabookaos.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Version(
     val imageUrl: String = "",
     val androidForceVersion: String = "",
     val versionText: String = ""
-)
+) : Parcelable
