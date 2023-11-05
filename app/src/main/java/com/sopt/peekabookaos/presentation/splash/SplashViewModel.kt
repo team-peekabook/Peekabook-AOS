96f0c6c3c00b5136fb05e850230a0914bb693273
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-11-05 23:37:09 +0900

    #273[fix] VERSION 엔티티로 변경

--- app/src/main/java/com/sopt/peekabookaos/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/splash/SplashViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.peekabookaos.domain.entity.SplashState
+import com.sopt.peekabookaos.domain.entity.Version
 import com.sopt.peekabookaos.domain.usecase.GetSplashStateUseCase
 import com.sopt.peekabookaos.domain.usecase.GetVersionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,7 +18,7 @@ class SplashViewModel @Inject constructor(
     private val getSplashStateUseCase: GetSplashStateUseCase,
     private val getVersionUseCase: GetVersionUseCase
 ) : ViewModel() {
-    private val _versionName: MutableLiveData<String> = MutableLiveData()
+    var latestVersion = Version()
     private val _isForceUpdateStatus = MutableLiveData(false)
     val isForceUpdateStatus: LiveData<Boolean> = _isForceUpdateStatus
     lateinit var majorVersion: String
@@ -30,16 +31,20 @@ class SplashViewModel @Inject constructor(
     fun getSplashState(): SplashState = getSplashStateUseCase()
 
     fun getSplitVersion() = run {
-        val versionSpiltList = _versionName.value?.split(".")
-        majorVersion = versionSpiltList?.get(0) ?: ""
-        minorVersion = versionSpiltList?.get(1) ?: ""
+        val versionSpiltList = latestVersion.androidForceVersion.split(".")
+        majorVersion = versionSpiltList[0]
+        minorVersion = versionSpiltList[1]
     }
 
     private fun getVersion() {
         viewModelScope.launch {
             getVersionUseCase()
                 .onSuccess { response ->
-                    _versionName.value = response.androidForceVersion
+                    latestVersion = Version(
+                        response.imageUrl,
+                        response.androidForceVersion,
+                        response.versionText
+                    )
                     _isForceUpdateStatus.value = true
                 }
                 .onFailure { throwable ->
