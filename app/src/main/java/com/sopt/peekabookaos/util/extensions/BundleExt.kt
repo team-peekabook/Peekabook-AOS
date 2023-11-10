cc3d8d5de2fdbdee3503ede3e00e7579ccc9a96b
이빵주 <yungju9982@naver.com>
2023-11-10 13:00:00 +0900

    #278 [feat] serializable 확장함수 구현

--- app/src/main/java/com/sopt/peekabookaos/util/extensions/BundleExt.kt
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/BundleExt.kt
@@ -3,6 +3,7 @@ package com.sopt.peekabookaos.util.extensions
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import java.io.Serializable
 
 inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -10,5 +11,14 @@ inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? 
     } else {
         @Suppress("DEPRECATION")
         getParcelable(key) as? T
+    }
+}
+
+inline fun <reified T : Serializable> Bundle.getSerializableCompat(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getSerializable(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getSerializable(key) as? T
     }
 }
