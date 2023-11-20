fce48a3c5002d510672551d7571267793b57d841
이빵주 <yungju9982@naver.com>
2023-11-20 13:00:00 +0900

    #278 [feat] getScreenSize 확장함수 구현

--- app/src/main/java/com/sopt/peekabookaos/util/extensions/ContextExt.kt
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/ContextExt.kt
@@ -3,7 +3,12 @@ package com.sopt.peekabookaos.util.extensions
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.DisplayMetrics
+import android.util.Size
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.sopt.peekabookaos.util.OnSingleClickListener
 
 fun View.setSingleOnClickListener(onSingleClick: (View) -> Unit) {
@@ -21,4 +26,28 @@ fun Context.isNetworkConnected(): Boolean {
             )
     }
     return isConnected
+}
+
+fun Context.getScreenSize(): Pair<Int, Int> {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val metrics = this.getSystemService(WindowManager::class.java).currentWindowMetrics
+        val windowInsets = metrics.windowInsets
+
+        val insets = windowInsets.getInsetsIgnoringVisibility(
+            WindowInsets.Type.systemBars() or WindowInsets.Type.displayCutout()
+        )
+        val insetsWidth = insets.right + insets.left
+        val insetsHeight = insets.top + insets.bottom
+
+        val legacySize = Size(
+            metrics.bounds.width() - insetsWidth,
+            metrics.bounds.height() - insetsHeight
+        )
+        Pair(legacySize.width, legacySize.height)
+    } else {
+        val metrics = DisplayMetrics()
+        @Suppress("DEPRECATION")
+        this.getSystemService(WindowManager::class.java).defaultDisplay.getRealMetrics(metrics)
+        Pair(metrics.widthPixels, metrics.heightPixels)
+    }
 }
