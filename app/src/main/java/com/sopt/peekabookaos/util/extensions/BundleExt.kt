ba0bd6e0dcc78c8be121de5558f7730cca7f5a5a
이빵주 <yungju9982@naver.com>
2023-11-28 06:54:30 +0900

    #278 [refactor] parcelable 확장함수 수정

--- app/src/main/java/com/sopt/peekabookaos/util/extensions/BundleExt.kt
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/BundleExt.kt
@@ -2,11 +2,13 @@ package com.sopt.peekabookaos.util.extensions
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
 
-fun <T> Bundle.getParcelableCompat(name: String, clazz: Class<T>): T? {
+inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelable(name, clazz)
+        getParcelable(key, T::class.java)
     } else {
-        getParcelable(name)
+        @Suppress("DEPRECATION")
+        getParcelable(key) as? T
     }
 }
