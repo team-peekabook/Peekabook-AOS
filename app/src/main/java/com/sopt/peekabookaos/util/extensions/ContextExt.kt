9d4231236026e34c40699cbc12a5e764f1eb8f7a
이빵주 <yungju9982@naver.com>
2023-12-03 13:00:00 +0900

    #278 [refactor] 네트워크에러 확장함수 수정

--- app/src/main/java/com/sopt/peekabookaos/util/extensions/ContextExt.kt
+++ app/src/main/java/com/sopt/peekabookaos/util/extensions/ContextExt.kt
@@ -20,12 +20,17 @@ fun Context.isNetworkConnected(): Boolean {
     val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
     if (capabilities != null) {
-        isConnected =
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(
-                NetworkCapabilities.TRANSPORT_CELLULAR
-            )
+        isConnected = isConnectedToWiFi(capabilities) || isConnectedToCellular(capabilities)
     }
     return isConnected
+}
+
+private fun isConnectedToWiFi(capabilities: NetworkCapabilities): Boolean {
+    return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+}
+
+private fun isConnectedToCellular(capabilities: NetworkCapabilities): Boolean {
+    return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
 }
 
 fun Context.getScreenSize(): Pair<Int, Int> {
