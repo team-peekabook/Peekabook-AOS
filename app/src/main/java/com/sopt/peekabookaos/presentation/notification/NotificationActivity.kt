202c7a2edab7d1fe5bf738ad080467d2a9f05e4e
이빵주 <yungju9982@naver.com>
2025-06-09 08:31:29 +0900

    fix: Notification Navigation 초기화하는 로직 제거

--- app/src/main/java/com/sopt/peekabookaos/presentation/notification/NotificationActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/presentation/notification/NotificationActivity.kt
@@ -1,7 +1,6 @@
 package com.sopt.peekabookaos.presentation.notification
 
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 import com.sopt.peekabookaos.R
 import com.sopt.peekabookaos.databinding.ActivityNotificationBinding
 import com.sopt.peekabookaos.util.binding.BindingActivity
@@ -10,17 +9,10 @@ import com.sopt.peekabookaos.util.extensions.activityOpenTransition
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NotificationActivity : BindingActivity<ActivityNotificationBinding>(R.layout.activity_notification) {
+class NotificationActivity :
+    BindingActivity<ActivityNotificationBinding>(R.layout.activity_notification) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initNotificationFragmentView()
-    }
-
-    private fun initNotificationFragmentView() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_notification) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        navController.navigate(R.id.notificationFragment)
         activityOpenTransition(R.anim.anim_from_bottom, R.anim.anim_to_top)
     }
 
