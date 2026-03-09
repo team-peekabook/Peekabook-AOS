a74c9d51910fec431002f80ff126657ebd23e699
yjlee <vilee@tjmedia.com>
2026-03-09 14:23:08 +0900

    feat: edge to edge 대응

--- app/src/main/java/com/sopt/peekabookaos/util/binding/BindingActivity.kt
+++ app/src/main/java/com/sopt/peekabookaos/util/binding/BindingActivity.kt
@@ -1,8 +1,11 @@
 package com.sopt.peekabookaos.util.binding
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -13,7 +16,14 @@ abstract class BindingActivity<T : ViewDataBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, layoutRes)
         binding.lifecycleOwner = this
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
